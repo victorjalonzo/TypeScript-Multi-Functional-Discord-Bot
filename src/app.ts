754af90ac2714dc8da3d1dde498b17c0dffb7340
VJA <victorj.alonzo@gmail.com>
2024-07-21 19:19:55 -0400

    removed services and controllers from the app and move to a container

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,9 @@
 import { DiscordAdapter } from "./Bot/adapters/discordAdapter.js";
 import { Client, GatewayIntentBits } from "discord.js";
 import { Config } from './shared/config/config.js'
-import { Database } from "./shared/intraestructure/Database.js";
-
-import { MongoRepository } from "./shared/intraestructure/MongoRepository.js";
-
-import { ChannelModel } from "./Channel/infrastructure/ChannelSchema.js";
-import { ChannelService } from "./Channel/application/ChannelService.js";
-import { ChannelController } from "./Channel/infrastructure/ChannelController.js";
-
-import { GuildModel } from "./Guild/intrastructure/GuildSchema.js";
-import { GuildService } from "./Guild/application/GuildService.js";
-import { GuildController } from "./Guild/intrastructure/GuildController.js";
-
+import { Controllers } from "./shared/intraestructure/Container.js";
 
 const main = async () => {
-    await Database.connect()
-
-    const guildRepository = new MongoRepository(GuildModel)
-    const guildService = new GuildService(guildRepository)
-    const guildController = new GuildController(guildService)
-
-    const channelRepository = new MongoRepository(ChannelModel)
-    const channelService = new ChannelService(channelRepository)
-    const channelController = new ChannelController(channelService)
-    
-    const controllers = {
-        guildController,
-        channelController
-    }
-
     const client = new Client({
         intents: [
             GatewayIntentBits.Guilds,
@@ -40,7 +14,7 @@ const main = async () => {
     const discordAdapter = new DiscordAdapter({
         client: client, 
         token: Config.discord.token,
-        controllers: controllers
+        controllers: Controllers
     })
     
     await discordAdapter.start()
