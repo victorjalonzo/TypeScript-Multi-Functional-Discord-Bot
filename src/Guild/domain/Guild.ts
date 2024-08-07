8cc1c335733717ffd88376ca217d889290132f5e
VJA <victorj.alonzo@gmail.com>
2024-08-06 23:39:14 -0400

    Guild updated

--- src/Guild/domain/Guild.ts
+++ src/Guild/domain/Guild.ts
@@ -1,6 +1,7 @@
 import { IGuild } from "./IGuild.js";
 import { ICasualPayment } from "../../CasualPayment/domain/ICasualPayment.js";
 import { ICredit } from "../../Credit/domain/ICredit.js";
+import { IPaypoint } from "../../Paypoint/domain/IPaypoint.js";
 
 export class Guild implements IGuild {
     constructor(
@@ -8,6 +9,7 @@ export class Guild implements IGuild {
         public name: string,
         public icon: string | null,
         public createdAt: Date,
+        public paypoints: IPaypoint[] = [],
         public casualPayments: ICasualPayment[] = [],
         public credits: ICredit[] = []
     ){}
