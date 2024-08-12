6ed63fadee1f6e64c9027a67f4718f9749dc379a
VJA <victorj.alonzo@gmail.com>
2024-08-11 23:53:38 -0400

    Invite modified

---
+++ src/Invite/infrastructure/Embeds/inviteEmbed.ts
@@ -0,0 +1,74 @@
+import { AttachmentBuilder, EmbedBuilder } from "discord.js"
+import { Asset } from "../../../shared/intraestructure/Asset.js"
+import { InlineBlockText } from "../../../shared/utils/textFormating.js"
+import { BarProgress } from "../../../shared/utils/BarProgress.js"
+import { Font, RankCardBuilder } from "canvacord"
+
+interface IProps {
+    displayName: string
+    username: string
+    avatarURL?: string
+    invitesCount: number
+    invitesRequired: number
+}
+
+export const createInviteCard = async (props: IProps) => {
+    const { displayName, username, avatarURL, invitesCount, invitesRequired } = props
+
+    Font.loadDefault();
+
+    const card = new RankCardBuilder()
+    .setDisplayName(displayName) // Big name
+    .setUsername(username) // small name, do not include it if you want to hide it
+    .setCurrentXP(invitesCount) // current xp
+    .setRequiredXP(invitesRequired) // required xp
+    //.setLevel(1200) // user level
+    //.setRank(160) // user rank
+    //.setOverlay(90) // overlay percentage. Overlay is a semi-transparent layer on top of the background
+    //.setBackground("#23272a") // set background color or,
+    //.setBackground("https://www.colorhexa.com/1e2124.png") // set background image
+    .setStatus("online"); // user status. Omit this if you want to hide it
+
+    if (avatarURL) card.setAvatar(avatarURL)
+
+    card.setStyles({
+        progressbar: {
+            thumb: {style: {backgroundColor: "#5ce65c"}}
+        },
+
+        statistics: {
+          container: {style: {fontWeight: "bold"}},
+          xp: {
+            container: { style: {margin: "0px", fontSize: "20px"}},
+            text: {style: {fontWeight: "bold", color: "#ffff"}}
+          }
+        }
+    });
+
+    card.setTextStyles({
+        level: "YOUR CREDITS :", // Custom text for the level
+        xp: "INVITES:", // Custom text for the experience points
+        rank: "CHALLENGE :", // Custom text for the rank
+      });
+
+    const image = await card.build({format: "png"});
+    return new AttachmentBuilder(image, {name: "rank.png"})
+}
+
+
+export const createInvitesEmbed = (username: string, avatarURL?: string, invitesCount: number = 0): EmbedBuilder => {
+    const askedBy = `Asked by @${username}`
+    const bar = BarProgress.create(invitesCount, invitesCount)
+    const description = `**\ninvites\n${bar}** ${invitesCount}/${invitesCount}`
+
+    const embed = new EmbedBuilder()
+    .setAuthor({ name: askedBy, iconURL: avatarURL })
+    .setDescription(description)
+    .setColor(0x6192e6)
+
+    if (avatarURL) embed.setThumbnail(avatarURL)
+    return embed
+}
+
+const InvitesWithRoleRewardEmbed = async () => {}
+const InvitesWithCreditRewardEmbed = async () => {}
---
+++ src/Invite/infrastructure/InviteCommandActions.ts
@@ -0,0 +1,107 @@
+import { IMemberInput } from "../../Member/domain/IMemberInput.js";
+import { ChatInputCommandInteraction } from "discord.js";
+import { InlineBlockText } from "../../shared/utils/textFormating.js";
+import { EmbedResult } from "../../shared/intraestructure/EmbedResult.js";
+import { createInviteCard } from "./Embeds/inviteEmbed.js";
+import { IRewardRoleInput } from "../../RewardRole/domain/IRewardRoleInput.js";
+import { IRewardRole } from "../../RewardRole/domain/IRewardRole.js";
+
+export class InviteCommandActions {
+    constructor(private memberService: IMemberInput, private rewardRoleService: IRewardRoleInput) {}
+
+    execute = async (interaction: ChatInputCommandInteraction) => {
+        try {
+            const guild = interaction.guild
+            let user = interaction.options.getUser('user')
+    
+            if (!guild) throw new Error("The guild was not found")
+            if (!user) user = interaction.user
+
+            await interaction.deferReply()
+
+            let invitesCount = 0
+
+            const memberCountResult = await this.memberService.getInviteMembersCount(user.id, guild.id)
+            if (!memberCountResult.isSuccess()) throw new Error(memberCountResult.error)
+
+            invitesCount = memberCountResult.value
+
+            const rewardRolesResult = await this.rewardRoleService.getAll(guild.id)
+            if (!rewardRolesResult.isSuccess()) throw new Error(rewardRolesResult.error)
+            
+            const rewardRoles = rewardRolesResult.value
+
+            const { 
+                isChallengeActive, 
+                isChallengeCompleted, 
+                invitesRequired, 
+                currentInvites,
+                roleId
+            } = await this.determineRoleChallenge(invitesCount, rewardRoles)
+
+            let description = ""
+
+            if (!isChallengeActive) {
+                description = `You have ${currentInvites} invites.`
+            }
+            else {
+                const role = guild.roles.cache.get(<string>roleId)
+
+                if (isChallengeCompleted) {
+                    description = `You have completed the challenge.`
+                }
+                else {
+                    const left = <number>invitesRequired - currentInvites
+                    description = `You need ${left} more invites to complete the ${role?.name} challenge.`
+                }
+            }
+            
+            const attachment = await createInviteCard({
+                displayName: user.username,
+                username: user.username,
+                avatarURL: user.avatarURL() ?? undefined,
+                invitesCount: currentInvites,
+                invitesRequired: <number>invitesRequired
+            })
+
+            return await interaction.editReply({content: description, files: [attachment]})
+        }
+        catch (e) {
+            console.log(e)
+            return await EmbedResult.fail({description: InlineBlockText(String(e)), interaction})
+        }
+
+    }
+
+    determineRoleChallenge = async (currentInvites: number, rewardRoles: IRewardRole[]) => {
+        let isChallengeActive = false
+        let isChallengeCompleted = false
+        let invitesRequired: number | undefined
+        let roleId: string | undefined
+    
+        if (rewardRoles.length === 0) {
+            invitesRequired = currentInvites
+        }
+        else {
+            isChallengeActive = true
+
+            const sortedRewardRoles = rewardRoles.sort((a, b) => a.invites - b.invites)
+    
+            for (const reward of sortedRewardRoles) {
+                if (reward.invites > currentInvites) {
+                    invitesRequired = <number>reward.invites
+                    roleId = reward.roleId
+                    break
+                }
+                if (reward == sortedRewardRoles[sortedRewardRoles.length-1]) {
+                    invitesRequired = <number>reward.invites
+                    currentInvites = invitesRequired
+                    roleId = reward.roleId
+                    isChallengeCompleted = true
+                }
+            }
+        }
+    
+        return {isChallengeActive, isChallengeCompleted, invitesRequired, currentInvites, roleId }
+    }
+}
