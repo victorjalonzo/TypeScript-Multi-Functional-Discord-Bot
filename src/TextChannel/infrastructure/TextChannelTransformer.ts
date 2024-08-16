59d40fec77d2fbffb6be041d8db14c5276105cc1
VJA <victorj.alonzo@gmail.com>
2024-08-15 23:41:02 -0400

    TextChannel transformer updated

--- src/TextChannel/infrastructure/TextChannelTransformer.ts
+++ src/TextChannel/infrastructure/TextChannelTransformer.ts
@@ -1,16 +1,17 @@
 import { TextChannel as DiscordTextChannel } from "discord.js";
 import { TextChannel } from "../domain/TextChannel.js";
 import { cache } from "../../shared/intraestructure/Cache.js";
-import { Channel } from "../../shared/intraestructure/Channel.js";
+import { ChannelUtility } from "../../shared/utils/ChannelUtility.js";
+import { CachedGuildNotFoundError } from "../../shared/domain/CachedGuildException.js";
 
 export class TextChannelTransformer {
     static parse = (incomeTextChannel: DiscordTextChannel): TextChannel => {
         const { name, id, position, parentId, guildId } = incomeTextChannel;
+
         const cachedGuild = cache.get(incomeTextChannel.guild.id);
+        if (!cachedGuild) throw new CachedGuildNotFoundError();
 
-        if (!cachedGuild) throw new Error("The cached guild was not found")
-
-        const permissionOverwrites = Channel.getPermissionOverwrites(incomeTextChannel);
+        const permissionOverwrites = ChannelUtility.getPermissionOverwrites(incomeTextChannel);
         
         return new TextChannel(id, name, position, permissionOverwrites, parentId, guildId, cachedGuild);
     }
