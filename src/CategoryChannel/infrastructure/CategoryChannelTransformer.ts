4897c04a7acf370fd9f57ce91e9a5a9b9d522f8b
VJA <victorj.alonzo@gmail.com>
2024-08-15 23:45:09 -0400

    CategoryChannel transformer updated

--- src/CategoryChannel/infrastructure/CategoryChannelTransformer.ts
+++ src/CategoryChannel/infrastructure/CategoryChannelTransformer.ts
@@ -3,18 +3,18 @@ import { ICategoryChannel } from "../domain/ICategoryChannel.js"
 import { CategoryChannel } from "../domain/CategoryChannel.js"
 import { cache } from "../../shared/intraestructure/Cache.js"
 import { CategoryChannelTransformationError } from "../domain/CategoryChannelExceptions.js"
-import { PermissionOverwrite } from "../../shared/domain/IPermissionOverwrite.js"
-import { Channel } from "../../shared/intraestructure/Channel.js"
+import { ChannelUtility } from "../../shared/utils/ChannelUtility.js"
+import { CachedGuildNotFoundError } from "../../shared/domain/CachedGuildException.js"
 
 export class CategoryChannelTransformer {
     static parse = (categoryChannel: DiscordCategoryChannel): ICategoryChannel => {
         try {
             const { id, name, position, guildId } = categoryChannel
+            
             const cachedGuild = cache.get(categoryChannel.guild.id)
-    
-            if (!cachedGuild) throw new Error("The cached guild was not found")
+            if (!cachedGuild) throw new CachedGuildNotFoundError();
 
-            const permissionOverwrites = Channel.getPermissionOverwrites(categoryChannel)
+            const permissionOverwrites = ChannelUtility.getPermissionOverwrites(categoryChannel)
     
             return new CategoryChannel(id, name, position, permissionOverwrites, [], guildId, cachedGuild)
         }
