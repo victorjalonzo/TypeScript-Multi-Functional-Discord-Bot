01a4380ed2530bcf8ba0464917cffa7bf8d150f6
VJA <victorj.alonzo@gmail.com>
2024-08-17 23:33:09 -0400

    MongoRepository updated: populate options was added for string and arrays

--- src/shared/intraestructure/MongoRepository.ts
+++ src/shared/intraestructure/MongoRepository.ts
@@ -4,12 +4,32 @@ import { IRepository } from '../domain/IRepository.js';
 export class MongoRepository<T extends Document> implements IRepository<T> {
     constructor(private readonly model: Model<T>) {}
 
-    async getAll(filter: Record<string, any> = {}): Promise<T[]> {
-        return await this.model.find(filter)
+    async getAll(filter: Record<string, any> = {}, populate: string | string[] | undefined = undefined): Promise<T[]> {
+        if (!populate) return await this.model.find(filter).exec();
+
+        if (!Array.isArray(populate)) return await this.model.find(filter).populate(populate).exec()
+
+        let query = this.model.find(filter);
+
+        populate.forEach((field) => {
+            query = query.populate(field);
+        });
+
+        return await query.exec();
     }
 
-    async get(filter: Record<string, any>): Promise<T | null> {
-        return await this.model.findOne(filter)
+    async get(filter: Record<string, any> = {}, populate: string | string[] | undefined = undefined): Promise<T | null> {
+        if (!populate) return await this.model.findOne(filter)
+        
+        if (!Array.isArray(populate)) return await this.model.findOne(filter).populate(populate).exec()
+
+        let query = this.model.findOne(filter);
+        
+        populate.forEach((field) => {
+            query = query.populate(field);
+        });
+
+        return await query.exec();
     }
 
     async create(data: T): Promise<T> {
