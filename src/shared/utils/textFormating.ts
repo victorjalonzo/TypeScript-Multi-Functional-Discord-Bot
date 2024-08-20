11dae8f0a81c2584e167caf0eafbbd9113ea9ef2
VJA <victorj.alonzo@gmail.com>
2024-08-19 21:19:09 -0400

    formatting for Italic, Bold and Simple block text was added

--- src/shared/utils/textFormating.ts
+++ src/shared/utils/textFormating.ts
@@ -1,3 +1,15 @@
 export const InlineBlockText = (text: string): string => {
     return `\`\`\`\n${text}\n\`\`\``
 }
+
+export const SimpleBlockText = (text: string): string => {
+    return `\`${text}\``;
+};
+
+export const BoldText = (text: string): string => {
+    return `**${text}**`;
+}
+
+export const ItalicText = (text: string): string => {
+    return `_${text}_`;
+}
