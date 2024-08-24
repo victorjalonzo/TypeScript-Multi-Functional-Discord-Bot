c58312eca96433110f3b03c9137208f6e337c70d
VJA <victorj.alonzo@gmail.com>
2024-08-23 23:40:06 -0400

    includes string array as a value type in the populate options.

--- src/shared/domain/IRepository.ts
+++ src/shared/domain/IRepository.ts
@@ -2,6 +2,6 @@ export interface IRepository <T> {
     create(data: Record<string, any>): Promise<T>
     update(filters: Record<string, any>, data: Record<string, any>): Promise<T | null>
     delete(filters: Record<string, any>): Promise<T | null>
-    get(filters?: Record<string, any>, populate?: string): Promise<T | null>
-    getAll(filters?: Record<string, any>, populate?: string): Promise<T[]>
+    get(filters?: Record<string, any>, populate?: string[] | string): Promise<T | null>
+    getAll(filters?: Record<string, any>, populate?: string[] | string): Promise<T[]>
 }
