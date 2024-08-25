d91cebfaf29b83a8b3cbb582095cbb21fbf8961f
VJA <victorj.alonzo@gmail.com>
2024-08-24 23:44:44 -0400

    Change arguments type

--- src/PaypointRole/domain/IPaypointInput.ts
+++ src/PaypointRole/domain/IPaypointInput.ts
@@ -3,6 +3,7 @@ import { Result } from "../../shared/domain/Result.js";
 
 export interface IPaypointInput {
     create(paypoint: IPaypoint): Promise<Result<IPaypoint>>
+    update(paypoint: IPaypoint): Promise<Result<IPaypoint>>
     get(id: string): Promise<Result<IPaypoint>>
     getAll(guildId: string): Promise<Result<IPaypoint[]>>
     delete(id: string, guildId: string): Promise<Result<IPaypoint>>
