2ac6079318a3fb3fc2b874ac9606baac06548abe
VJA <victorj.alonzo@gmail.com>
2024-08-31 23:41:46 -0400

    updated

---
+++ src/PaypointRole/infrastructure/Embeds/CheckYourDMEmbed.ts
@@ -0,0 +1,18 @@
+import { ActionRowBuilder, AttachmentBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } from "discord.js"
+import { Asset } from "../../../shared/intraestructure/Asset.js"
+import { InlineBlockText } from "../../../shared/utils/textFormating.js"
+
+export const createCheckYourDMEmbed = async (): Promise<{embed: EmbedBuilder, files: AttachmentBuilder[]}> => {
+    const files: AttachmentBuilder[] = []
+
+    const icon = await Asset.get('mail')
+    
+    files.push(icon.attachment)
+
+    const embed = new EmbedBuilder()
+    .setThumbnail(icon.attachmentURL)
+    .setDescription(`${InlineBlockText('I Sent You a Message,\nCheck Your DM Please.')}`)
+    .setColor(0xe6e8ee)
+    
+    return {embed, files}
+}
