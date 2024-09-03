2d4b0c91855bf2845d7872c8aed18271e2735c03
VJA <victorj.alonzo@gmail.com>
2024-09-03 11:11:30 -0400

    Added product field to the schema and remove amount field from it

--- src/CasualTransaction/infrastructure/CasualTransactionSchema.ts
+++ src/CasualTransaction/infrastructure/CasualTransactionSchema.ts
@@ -1,16 +1,17 @@
 import { Document, model, Schema} from "mongoose"
 import { ICasualTransaction } from "../domain/ICasualTransaction.js"
+import { CasualTransactionState } from "../domain/CasualTransactionStateEnums.js"
 
 const CasualTransactionSchema = new Schema<Document & ICasualTransaction>({
     id: { type: String, required: true, unique: true },
     member: { type: Schema.Types.ObjectId, ref: "Members", required: true },
     memberId: { type: String, required: true },
     guildId: { type: String, required: true },
-    state: { type: String, required: true },
+    state: { type: Number, required: true},
     paymentMethodName: { type: String, default: null },
     paymentMethodValue: { type: String, default: null },
+    product: { type: Schema.Types.ObjectId, ref: "RoleProducts"},
     paymentFrom: { type: String, default: null },
-    amount: { type: Number, default: null },
     invoices: { type: [Buffer], default: null },
     createAt: { type: Date, required: true },
     expiredAt: { type: Date, default: null }
