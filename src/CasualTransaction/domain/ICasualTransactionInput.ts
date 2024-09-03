1b72f689d4941ebdf042ec4e496e14a1ad5fa0a8
VJA <victorj.alonzo@gmail.com>
2024-09-03 11:10:56 -0400

    Added get and update as a methods to the interface.

--- src/CasualTransaction/domain/ICasualTransactionInput.ts
+++ src/CasualTransaction/domain/ICasualTransactionInput.ts
@@ -3,9 +3,11 @@ import { ICasualTransaction } from "./ICasualTransaction.js";
 
 export interface ICasualTransactionInput {
     create(casualTransaction: ICasualTransaction): Promise<Result<ICasualTransaction>>
+    get(id: string): Promise<Result<ICasualTransaction>>
     getAllByGuild(guildId: string): Promise<Result<ICasualTransaction[]>>
     getAllByMemberOnGuild(memberId: string, guildId: string): Promise<Result<ICasualTransaction[]>>
     getAllByMember(memberId: string): Promise<Result<ICasualTransaction[]>>
     getLastOneByMember(memberId: string): Promise<Result<ICasualTransaction>>
+    update(casualTransaction: ICasualTransaction): Promise<Result<ICasualTransaction>>
     delete(id: string): Promise<Result<ICasualTransaction>>
 }
