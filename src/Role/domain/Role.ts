86fb5355d6cb1158eac50cc9d2ed54a6a11b8b79
VJA <victorj.alonzo@gmail.com>
2024-09-04 20:08:02 -0400

    Updated the constructor to accept a object and change the property permissions to an array of strings

--- src/Role/domain/Role.ts
+++ src/Role/domain/Role.ts
@@ -1,19 +1,33 @@
 import { IRole } from "./IRole.js";
 import { IGuild } from "../../Guild/domain/IGuild.js";
 
+interface IProps extends Omit<IRole, "createdAt">{}
+
 export class Role implements IRole {
-    constructor (
-        public id: string,
-        public name: string,
-        public position: number,
-        public color: number,
-        public permissions: string,
-        public hoist: boolean,
-        public mentionable: boolean,
-        public managed : boolean,
-        public editable: boolean,
-        public guildId: string,
-        public guild: IGuild,
-        public createdAt: Date = new Date()
-    ){}
+    id: string
+    name: string
+    position: number
+    color: number
+    permissions: string[] = []
+    hoist: boolean
+    mentionable: boolean
+    managed : boolean
+    editable: boolean
+    guildId: string
+    guild: IGuild
+    createdAt: Date = new Date()
+
+    constructor (props: IProps){
+        this.id = props.id
+        this.name = props.name
+        this.position = props.position
+        this.color = props.color
+        this.permissions = props.permissions
+        this.hoist = props.hoist
+        this.mentionable = props.mentionable
+        this.managed = props.managed
+        this.editable = props.editable
+        this.guildId = props.guildId
+        this.guild = props.guild
+    }
 }
