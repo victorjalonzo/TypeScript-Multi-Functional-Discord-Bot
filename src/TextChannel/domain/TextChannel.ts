46eea3e5b72b49cd3762ed8e471d4c37996aeb46
VJA <victorj.alonzo@gmail.com>
2024-09-06 23:44:25 -0400

    Added the property parent and changed the property parentId to string

--- src/TextChannel/domain/TextChannel.ts
+++ src/TextChannel/domain/TextChannel.ts
@@ -1,16 +1,27 @@
 import {ITextChannel} from "./ITextChannel.js";
 import {IGuild } from "../../Guild/domain/IGuild.js";
 import {PermissionOverwrite} from "../../shared/domain/IPermissionOverwrite.js";
+import { ICategoryChannel } from "../../CategoryChannel/domain/ICategoryChannel.js";
 
 export class TextChannel implements ITextChannel {
-    constructor(
-        public id: string,
-        public name: string,
-        public position: number,
-        public permissionOverwrites: PermissionOverwrite[] | [],
-        public parentId: string | null,
-        public guildId: string,
-        public guild: IGuild,
-        public createdAt: Date = new Date(),
-    ) {}
+    id: string
+    name: string
+    position: number
+    permissionOverwrites: PermissionOverwrite[] | []
+    parent: ICategoryChannel | null
+    parentId: string | null
+    guildId: string
+    guild: IGuild
+    createdAt: Date = new Date()
+    
+    constructor(props: ITextChannel) {
+        this.id = props.id
+        this.name = props.name
+        this.position = props.position
+        this.permissionOverwrites = props.permissionOverwrites
+        this.parent = props.parent
+        this.parentId = props.parentId
+        this.guildId = props.guildId
+        this.guild = props.guild
+    }
 }
