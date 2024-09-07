97543b37dcdf37c3e5407ee7c06d5ea9728e9405
VJA <victorj.alonzo@gmail.com>
2024-09-06 23:55:03 -0400

    Added guild and category service as dependencies to the constructor, also updated the methods CRUD.

--- src/VoiceChannel/application/VoiceChannelService.ts
+++ src/VoiceChannel/application/VoiceChannelService.ts
@@ -2,6 +2,11 @@ import { IVoiceChannelInput } from "../domain/IVoiceChannelInput.js";
 import { IVoiceChannel } from "../domain/IVoiceChannel.js";
 import { IRepository } from "../../shared/domain/IRepository.js";
 import { Result } from "../../shared/domain/Result.js";
+import { ITextChannel } from "../../TextChannel/domain/ITextChannel.js";
+import { IGuild } from "../../Guild/domain/IGuild.js";
+import { IGuildInput } from "../../Guild/domain/IGuildInput.js";
+import { ICategoryChannelInput } from "../../CategoryChannel/domain/ICategoryChannelInput.js";
+import { ICategoryChannel } from "../../CategoryChannel/domain/ICategoryChannel.js";
 
 import {
     VoiceChannelCreationError,
@@ -10,11 +15,38 @@ import {
     VoiceChannelDeletionError
 } from "../domain/VoiceChannelExceptions.js";
 
+
 export class VoiceChannelService implements IVoiceChannelInput {
-    constructor (private repository: IRepository<IVoiceChannel>) {}
+    constructor (
+        private repository: IRepository<IVoiceChannel>,
+        private guildService: IGuildInput,
+        private categoryChannelService: ICategoryChannelInput
+    ) {}
+
+    async _getGuild (textChannel: ITextChannel): Promise<IGuild> {
+        const guildResult = await this.guildService.get(textChannel.guildId);
+        if (!guildResult.isSuccess()) throw guildResult.error
+
+        return guildResult.value
+    }
+
+    _getParent = async (textChannel: ITextChannel): Promise<ICategoryChannel | undefined> => {
+        if (!textChannel.parentId) return
+        
+        const categoryChannelResult = await this.categoryChannelService.get(textChannel.parentId, textChannel.guildId);
+        if (!categoryChannelResult.isSuccess()) return
+
+        return categoryChannelResult.value
+    }
 
     async create(voiceChannel: IVoiceChannel): Promise<Result<IVoiceChannel>> {
         try {
+            const guild = await this._getGuild(voiceChannel);
+            const parent = await this._getParent(voiceChannel);
+
+            voiceChannel.guild = guild
+            voiceChannel.parent = parent ?? null
+
             const createdVoiceChannel = await this.repository.create(voiceChannel);
             if (!createdVoiceChannel) throw new VoiceChannelCreationError()
             return Result.success(createdVoiceChannel);
@@ -48,6 +80,12 @@ export class VoiceChannelService implements IVoiceChannelInput {
 
     async update(voiceChannel: IVoiceChannel): Promise<Result<IVoiceChannel>> {
         try {
+            const guild = await this._getGuild(voiceChannel);
+            const parent = await this._getParent(voiceChannel);
+
+            voiceChannel.guild = guild
+            voiceChannel.parent = parent ?? null
+
             const updatedVoiceChannel = await this.repository.update({id: voiceChannel.id, guildId: voiceChannel.guildId}, voiceChannel);
             if (!updatedVoiceChannel) throw new VoiceChannelUpdateError()
 
