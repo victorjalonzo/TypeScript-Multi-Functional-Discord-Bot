845ed9c62f5f5d33ab93546f2ef450fe5a9481d3
VJA <victorj.alonzo@gmail.com>
2024-09-07 23:51:10 -0400

    Changed the getAll method param from paypointId to guildId

--- src/RoleProduct/application/RoleProductService.ts
+++ src/RoleProduct/application/RoleProductService.ts
@@ -7,22 +7,13 @@ import { IRoleProductInput } from "../domain/IRoleProductInput.js";
 
 export class RoleProductService implements IRoleProductInput {
     constructor(
-        private repository: IRepository<IRoleProduct>,
-        private paypointRepository: IRepository<IPaypoint>
+        private repository: IRepository<IRoleProduct>
     ) {}
 
     create = async (roleProduct: IRoleProduct): Promise<Result<IRoleProduct>> => {
         try {
             const createdRoleProduct = await this.repository.create(roleProduct);
             if (!createdRoleProduct) throw new RoleProductCreationError();
-
-            const { paypoint } = roleProduct
-            roleProduct.paypoint.products.push(createdRoleProduct);
-
-            const updatedPaypoint = await this.paypointRepository.update({id: paypoint.id}, {products: paypoint.products});
-            if (!updatedPaypoint) throw new Error("The paypoint could not be found");
-
-            createdRoleProduct.paypoint = updatedPaypoint
             
             return Result.success(createdRoleProduct);
         }
@@ -42,9 +33,9 @@ export class RoleProductService implements IRoleProductInput {
         }
     }
 
-    getAll = async (paypointId: string): Promise<Result<IRoleProduct[]>> => {
+    getAll = async (guildId: string): Promise<Result<IRoleProduct[]>> => {
         try {
-            const roleProducts = await this.repository.getAll({paypointId}, 'role');
+            const roleProducts = await this.repository.getAll({guildId}, 'role');
             return Result.success(roleProducts);
         }
         catch (e) {
@@ -56,11 +47,6 @@ export class RoleProductService implements IRoleProductInput {
         try {
             const deletedRoleProduct = await this.repository.delete({id});
             if (!deletedRoleProduct) throw new RoleProductDeletionError();
-
-            const { paypoint } = deletedRoleProduct
-            paypoint.products = paypoint.products.filter(product => product.id !== id);
-            const updatedPaypoint = await this.paypointRepository.update({id: paypoint.id}, {products: paypoint.products});
-            if (!updatedPaypoint) throw new Error("The paypoint could not be found");
             
             return Result.success(deletedRoleProduct);
         }
