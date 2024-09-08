326fe914fc5ee3df057894b185da94109aac4431
VJA <victorj.alonzo@gmail.com>
2024-09-07 23:50:34 -0400

    Change the getAll method param from paypointId to guildId

--- src/RoleProduct/domain/IRoleProductInput.ts
+++ src/RoleProduct/domain/IRoleProductInput.ts
@@ -3,7 +3,7 @@ import { Result } from "../../shared/domain/Result.js"
 
 export interface IRoleProductInput {
     create (roleProduct: IRoleProduct): Promise<Result<IRoleProduct>>
-    getAll (paypointId: string): Promise<Result<IRoleProduct[]>>
+    getAll (guildId: string): Promise<Result<IRoleProduct[]>>
     get (id: string): Promise<Result<IRoleProduct>>
     delete (id: string): Promise<Result<IRoleProduct>>
 }
