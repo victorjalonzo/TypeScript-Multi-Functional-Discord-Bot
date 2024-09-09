1c9dcfe3adce947b7690f460bf842712849006d3
VJA <victorj.alonzo@gmail.com>
2024-09-08 23:46:34 -0400

    Updated the constructor to receive an object.

--- src/Member/domain/Member.ts
+++ src/Member/domain/Member.ts
@@ -2,13 +2,21 @@ import { IMember } from "./IMember.js";
 import { IGuild } from "../../Guild/domain/IGuild.js";
 
 export class Member implements IMember {
-    constructor (
-        public id: string,
-        public username: string,
-        public discriminator: string,
-        public guildId: string,
-        public guild: IGuild,
-        public avatarURL?: string | null,
-        public invitedBy?: string | null
-    ){}
+    id: string
+    username: string
+    discriminator: string
+    guildId: string
+    guild: IGuild
+    avatarURL?: string | null
+    invitedBy?: string | null
+
+    constructor (props: IMember){
+        this.id = props.id
+        this.username = props.username
+        this.discriminator = props.discriminator
+        this.guildId = props.guildId
+        this.guild = props.guild
+        this.avatarURL = props.avatarURL
+        this.invitedBy = props.invitedBy
+    }
 }
