d52e211b47b4501814d7fb61695c96a236ab24fa
VJA <victorj.alonzo@gmail.com>
2024-09-08 23:39:29 -0400

    Removed the property roleId and added the property role. Also modify the constructor to receive a object.

--- src/RewardRole/domain/RewardRole.ts
+++ src/RewardRole/domain/RewardRole.ts
@@ -1,14 +1,20 @@
 import { IRewardRole } from "./IRewardRole.js";
 import { IGuild } from "../../Guild/domain/IGuild.js";
-import { createRandomId } from "../../shared/utils/generate.js";
+import { IRole } from "../../Role/domain/IRole.js";
 
 export class RewardRole implements IRewardRole {
-    constructor(
-        public roleId: string,
-        public invites: number,
-        public guildId: string,
-        public guild: IGuild,
-        public createdAt: Date = new Date(),
-        public id: string = createRandomId(),
-    ){}
+    public id: string
+    public role: IRole
+    public invites: number
+    public guildId: string
+    public guild: IGuild
+    public createdAt: Date = new Date()
+
+    constructor(props: Omit<IRewardRole, "createdAt">){
+        this.id = props.id
+        this.role = props.role
+        this.invites = props.invites
+        this.guildId = props.guildId
+        this.guild = props.guild
+    }
 }
