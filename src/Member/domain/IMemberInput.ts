3c37901292e63a113b77505fe43a2f2dd8a4fb4f
VJA <victorj.alonzo@gmail.com>
2024-09-08 23:45:40 -0400

    Added the method getAll to the interface.

--- src/Member/domain/IMemberInput.ts
+++ src/Member/domain/IMemberInput.ts
@@ -4,6 +4,7 @@ import { IMember } from "./IMember.js";
 export interface IMemberInput {
     create(member: IMember): Promise<Result<IMember>>
     update(data: IMember): Promise<Result<IMember>>
+    getAll(guildId: string): Promise<Result<IMember[]>>
     getInviteMembersCount(inviterId: string, guildId: string): Promise<Result<number>>
     get(id: string, guildId: string): Promise<Result<IMember>>
     delete(filters: Record<string, any>): Promise<Result<IMember>>
