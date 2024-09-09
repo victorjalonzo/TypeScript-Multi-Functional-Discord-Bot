4696e335424f9f1b67e0abdf6bf708bfa5931532
VJA <victorj.alonzo@gmail.com>
2024-09-08 23:38:19 -0400

    Removed the field roleId and added the field role

--- src/RewardRole/domain/IRewardRole.ts
+++ src/RewardRole/domain/IRewardRole.ts
@@ -1,8 +1,9 @@
 import { IGuild } from "../../Guild/domain/IGuild.js";
+import { IRole } from "../../Role/domain/IRole.js";
 
 export interface IRewardRole {
     id: string
-    roleId: string
+    role: IRole
     invites: number
     guildId: string
     guild: IGuild
