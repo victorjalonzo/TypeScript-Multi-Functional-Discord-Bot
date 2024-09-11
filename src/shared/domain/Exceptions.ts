0e0e20b5ed4494f05c7b86af3ef8eb29307270d4
VJA <victorj.alonzo@gmail.com>
2024-09-10 23:44:23 -0400

    Added an exception for the bot as guild member not found.

--- src/shared/domain/Exceptions.ts
+++ src/shared/domain/Exceptions.ts
@@ -27,3 +27,15 @@ export class UnknownStringSelectMenuInteractionError extends Error {
         super('The string select menu interaction was not found');
     }
 }
+
+export class BotMemberNotFoundError extends Error {
+    constructor() {
+        super('The bot member was not found');
+    }
+}
+
+export class BotMissingPermissionsError extends Error {
+    constructor() {
+        super('The bot does not have the required permissions to realize this action.');
+    }
+}
