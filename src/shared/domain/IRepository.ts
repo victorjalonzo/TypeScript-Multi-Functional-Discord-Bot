e7a80e2c8f92ca5444d655248244737953ffe05f
VJA <victorj.alonzo@gmail.com>
2024-09-10 23:45:35 -0400

    Added the method getAll to the interface.

--- src/shared/domain/IRepository.ts
+++ src/shared/domain/IRepository.ts
@@ -2,6 +2,7 @@ export interface IRepository <T> {
     create(data: Record<string, any>): Promise<T>
     update(filters: Record<string, any>, data: Record<string, any>): Promise<T | null>
     delete(filters: Record<string, any>): Promise<T | null>
+    deleteAll(filters: Record<string, any>): Promise<T[]>
     get(filters?: Record<string, any>, populate?: string[] | string): Promise<T | null>
     getAll(filters?: Record<string, any>, populate?: string[] | string): Promise<T[]>
 }
