11262b88f96495c3eaec8849ee4ff95747e9194c
VJA <victorj.alonzo@gmail.com>
2024-09-12 21:50:24 -0400

    Added the getByMessageID method

--- src/PaypointRole/domain/IPaypointInput.ts
+++ src/PaypointRole/domain/IPaypointInput.ts
@@ -4,7 +4,8 @@ import { Result } from "../../shared/domain/Result.js";
 export interface IPaypointInput {
     create(paypoint: IPaypoint): Promise<Result<IPaypoint>>
     update(paypoint: IPaypoint): Promise<Result<IPaypoint>>
-    get(id: string): Promise<Result<IPaypoint>>
+    get(guildId: string): Promise<Result<IPaypoint>>
+    getByMessageID(messageID: string): Promise<Result<IPaypoint>>
     getAll(guildId: string): Promise<Result<IPaypoint[]>>
     delete(guildId: string): Promise<Result<IPaypoint>>
 }
