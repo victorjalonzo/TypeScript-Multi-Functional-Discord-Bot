04a2ebe2e5474a65206cd8f39960c2ef9e7b6c2d
VJA <victorj.alonzo@gmail.com>
2024-09-14 22:54:45 -0400

    Added news properties.

--- src/Guild/domain/Guild.ts
+++ src/Guild/domain/Guild.ts
@@ -2,17 +2,33 @@ import { IGuild } from "./IGuild.js";
 import { ICasualPayment } from "../../CasualPayment/domain/ICasualPayment.js";
 import { ICredit } from "../../Credit/domain/ICredit.js";
 import { IPaypoint } from "../../PaypointRole/domain/IPaypointRole.js";
+import { IRole } from "../../Role/domain/IRole.js";
 
 export class Guild implements IGuild {
-    inviteData: unknown
+    public id: string
+    public name: string
+    public icon: string | null
+    public createdAt: Date = new Date()
+    public paypoints: IPaypoint[] = []
+    public casualPayments: ICasualPayment[] = []
+    public credits: ICredit[] = []
+    public inviteData: unknown
+    public defaultCredits: number = 0
+    public defaultRole?: IRole | null = null
+    public defaultNotificationChannel?: IRole | null = null
+    public defaultInvoiceChannel?: IRole | null = null
 
-    constructor(
-        public id: string,
-        public name: string,
-        public icon: string | null,
-        public createdAt: Date,
-        public paypoints: IPaypoint[] = [],
-        public casualPayments: ICasualPayment[] = [],
-        public credits: ICredit[] = []
-    ){}
+    constructor(props: Omit<IGuild, 'inviteData'>){
+        this.id = props.id
+        this.name = props.name
+        this.icon = props.icon
+        this.createdAt = props.createdAt
+        this.paypoints = props.paypoints
+        this.casualPayments = props.casualPayments
+        this.credits = props.credits
+        this.defaultCredits = props.defaultCredits
+        this.defaultRole = props.defaultRole
+        this.defaultNotificationChannel = props.defaultNotificationChannel
+        this.defaultInvoiceChannel = props.defaultInvoiceChannel
+    }
 }
