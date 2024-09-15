1a915b31fab519ca7587e856638bc9df37f2a93e
VJA <victorj.alonzo@gmail.com>
2024-09-15 19:55:36 -0400

    Added exceptions for credits not founds.

--- src/Credit/domain/CreditExceptions.ts
+++ src/Credit/domain/CreditExceptions.ts
@@ -1,3 +1,33 @@
+export class CreditCreationError extends Error {
+    constructor() {
+        super('The credit could not be created');
+    }
+}
+
+export class CreditUpdateError extends Error {
+    constructor() {
+        super('The credit could not be updated');
+    }
+}
+
+export class CreditNotFoundError extends Error {
+    constructor() {
+        super('The credit could not be found');
+    }
+}
+
+export class CreditProductsNotFoundError extends Error {
+    constructor() {
+        super('The credit products could not be found');
+    }
+}
+
+export class CreditDeletionError extends Error {
+    constructor() {
+        super('The credit could not be deleted');
+    }
+}
+
 export class PriceNotProvidedError extends Error {
     constructor() {
         super('The price for the credit was not provided');
