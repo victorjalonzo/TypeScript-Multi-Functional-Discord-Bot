2ca637ed65ae4e165acfce7ff4cc1aba6807e01d
VJA <victorj.alonzo@gmail.com>
2024-09-15 19:53:53 -0400

    Add the properties id, description, media and codec to the class.

--- src/Credit/domain/Credit.ts
+++ src/Credit/domain/Credit.ts
@@ -1,12 +1,25 @@
 import { ICredit } from "./ICredit.js";
 import { IGuild } from "../../Guild/domain/IGuild.js";
+import { createRandomId } from "../../shared/utils/generate.js";
 
 export class Credit implements ICredit {
-    constructor(
-        public price: number,
-        public amount: number,
-        public guildId: string,
-        public guild: IGuild,
-        public createdAt: Date
-    ) {}
+    public id: string = createRandomId()
+    public price: number
+    public amount: number
+    public media?: Buffer | null
+    public codec?: string | null
+    public description?: string | null
+    public guildId: string
+    public guild: IGuild
+    public createdAt: Date = new Date()
+
+    constructor(props: Omit<ICredit, "id" | "createdAt">) {
+        this.price = props.price
+        this.amount = props.amount
+        this.media = props.media
+        this.codec = props.codec
+        this.description = props.description
+        this.guildId = props.guildId
+        this.guild = props.guild
+    }
 }
