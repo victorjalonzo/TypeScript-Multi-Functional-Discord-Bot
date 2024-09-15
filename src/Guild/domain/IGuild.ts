09d65ca47a643868e986d73ce67e11316df86ea0
VJA <victorj.alonzo@gmail.com>
2024-09-14 22:54:23 -0400

    Added new fields

--- src/Guild/domain/IGuild.ts
+++ src/Guild/domain/IGuild.ts
@@ -1,13 +1,17 @@
 import { ICasualPayment } from "../../CasualPayment/domain/ICasualPayment.js";
 import { ICredit } from "../../Credit/domain/ICredit.js";
 import { IPaypoint } from "../../PaypointRole/domain/IPaypointRole.js";
+import { IRole } from "../../Role/domain/IRole.js";
 
 export interface IGuild  {
     id: string
     name: string
     icon: string | null
+    defaultCredits: number
+    defaultRole?: IRole | null
+    defaultNotificationChannel?: IRole | null
+    defaultInvoiceChannel?: IRole | null
     createdAt: Date
-    
     paypoints: IPaypoint[]
     casualPayments: ICasualPayment[]
     credits: ICredit[]
