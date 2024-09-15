2322942076ba0f293f47a8ad44c429825fa88b4c
VJA <victorj.alonzo@gmail.com>
2024-09-14 22:55:10 -0400

    Updated the method update.

--- src/Guild/domain/IGuildInput.ts
+++ src/Guild/domain/IGuildInput.ts
@@ -4,6 +4,6 @@ import { Result } from "../../shared/domain/Result.js"
 export interface IGuildInput {
     get (id: string): Promise<Result<IGuild>>
     create (guild: IGuild): Promise<Result<IGuild>>
-    update (oldGuild: IGuild, newGuild: IGuild): Promise<Result<IGuild>>
+    update (guild: IGuild): Promise<Result<IGuild>>
     delete (id: string): Promise<Result<Record<string, any>>>
 }
