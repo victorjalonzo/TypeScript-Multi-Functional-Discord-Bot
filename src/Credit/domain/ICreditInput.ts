53ca55b5a35b35fe263a3b23d82a100708598a05
VJA <victorj.alonzo@gmail.com>
2024-09-15 19:56:47 -0400

    Changed the methods arguments

--- src/Credit/domain/ICreditInput.ts
+++ src/Credit/domain/ICreditInput.ts
@@ -3,7 +3,8 @@ import { Result } from "../../shared/domain/Result.js";
 
 export interface ICreditInput {
     create(credit: ICredit): Promise<Result<ICredit>>
-    getAll(filters: Record<string, any>): Promise<Result<ICredit[]>>
-    get(filters: Record<string, any>): Promise<Result<ICredit>>
-    delete(filters: Record<string, any>): Promise<Result<ICredit>>
+    getAll(guildId: string): Promise<Result<ICredit[]>>
+    get(id: string): Promise<Result<ICredit>>
+    delete(id: string): Promise<Result<ICredit>>
+    deleteAll(guildId: string): Promise<Result<ICredit[]>>
 }
