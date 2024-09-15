188af31dcb1e50adbf24ef254f86abb90c600054
VJA <victorj.alonzo@gmail.com>
2024-09-14 22:52:12 -0400

    Added a method to convert URLs into a Buffer.

--- src/shared/utils/AttachmentBuffer.ts
+++ src/shared/utils/AttachmentBuffer.ts
@@ -1,5 +1,6 @@
 import { Attachment } from "discord.js";
 import { AttachmentBuilder } from "discord.js";
+import sharp from "sharp";
 
 export const getBufferFromAttachment = async (attachment: Attachment): Promise<Buffer> => {
     const response = await fetch(attachment.url);
@@ -14,4 +15,16 @@ export const getAttachmentFromBuffer = async (buffer: Buffer, filename?: string)
     const attachment = new AttachmentBuilder(buffer, { name: filename });
     
     return attachment;
+}
+
+export const getBufferFromURL = async (url: string): Promise<Buffer> => {
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+  const convertedBuffer = await sharp(buffer)
+    .png()
+    .toBuffer();
+
+    return convertedBuffer;
 }
