f30cadfa8f800a9720aba16cb93e48c9160fcf85
VJA <victorj.alonzo@gmail.com>
2024-09-14 22:56:19 -0400

    Added news field to the schema.

--- src/Guild/infrastructure/GuildSchema.ts
+++ src/Guild/infrastructure/GuildSchema.ts
@@ -6,7 +6,10 @@ const guildSchema = new Schema<IGuild & Document>({
     id: { type: String, required: true, unique: true },
     icon: { type: String, default: null },
     createdAt: { type: Date, required: true },
-    
+    defaultRole: { type: Schema.Types.ObjectId, ref: "Roles", default: null },
+    defaultNotificationChannel: { type: Schema.Types.ObjectId, ref: "Roles", default: null },
+    defaultInvoiceChannel: { type: Schema.Types.ObjectId, ref: "Roles", default: null },
+    defaultCredits: { type: Number, default: 0 },
     casualPayments: [{type: Schema.Types.ObjectId, ref: "CasualPayments", required: true}],
     credits: [{type: Schema.Types.ObjectId, ref: "Credits", required: true}],
     paypoints: [{type: Schema.Types.ObjectId, ref: "Paypoints", required: true}]
