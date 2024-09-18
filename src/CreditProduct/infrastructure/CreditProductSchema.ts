6d65ea976fdc0dc6e9e4d4c385af7741a793514e
VJA <victorj.alonzo@gmail.com>
2024-09-17 22:58:05 -0400

    Added the schema.

---
+++ src/CreditProduct/infrastructure/CreditProductSchema.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { ICreditProduct } from '../domain/ICreditProduct.js';
+
+const creditProductSchema = new Schema<ICreditProduct & Document>({
+    name: { type: String, required: true },
+    id: { type: String, required: true },
+    price: { type: Number, required: true },
+    credits: { type: Number, required: true },
+    media: { type: Buffer, default: null },
+    mediaFilename: { type: String, default: null },
+    description: { type: String, default: null },
+    guildId: { type: String, required: true },
+    guild: { 
+        type: Schema.Types.ObjectId, 
+        ref: "Guilds", 
+        required: true 
+    }
+});
+
+export const CreditModel = mongoose.model<ICreditProduct & Document>('CreditProducts', creditProductSchema);
