b1bc83c4d12d8ae1361641899057beaf6a8cbb75
VJA <victorj.alonzo@gmail.com>
2024-09-17 23:06:51 -0400

    Added news exceptions.

--- src/RoleProduct/domain/RoleProductExceptions.ts
+++ src/RoleProduct/domain/RoleProductExceptions.ts
@@ -10,8 +10,33 @@ export class RoleProductNotFound extends Error {
     }
 }
 
+export class RoleProductsNotFound extends Error {
+    constructor() {
+        super('The role products were not found');
+    }
+}
+
 export class RoleProductDeletionError extends Error {
     constructor() {
         super('The role product could not be deleted');
+    }
+}
+
+
+export class RoleProductNotPrividedError extends Error {
+    constructor() {
+        super('The role product was not provided');
+    }
+}
+
+export class RoleProductPriceNotPrividedError extends Error {
+    constructor() {
+        super('The price for the role product was not provided');
+    }
+}
+
+export class RoleProductInvalidPriceError extends Error {
+    constructor() {
+        super('The price for the role product is invalid');
     }
 }
