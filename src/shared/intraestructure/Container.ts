965d9722d507d0da2810fe41b2b49f77f1218c1f
VJA <victorj.alonzo@gmail.com>
2024-09-17 23:06:02 -0400

    Added new modules dependencies.

--- src/shared/intraestructure/Container.ts
+++ src/shared/intraestructure/Container.ts
@@ -2,21 +2,21 @@ import { Database } from "./Database.js";
 import { MongoRepository } from "./MongoRepository.js";
 import { SlashCommandCallable } from '../../shared/intraestructure/SlashCommandCallable.js';
 
-import { PaypointModel } from "../../PaypointRole/infrastructure/PaypointSchema.js";
-import { PaypointService } from "../../PaypointRole/application/PaypointService.js";
-import { PaypointCommand } from '../../PaypointRole/infrastructure/PaypointCommand.js';
-import { PaypointCommandActions } from "../../PaypointRole/infrastructure/PaypointCommandActions.js";
-import { PaypointComponentActions } from "../../PaypointRole/infrastructure/PaypointComponentActions.js";
+import { PaypointModel } from "../../Paypoint/infrastructure/PaypointSchema.js";
+import { PaypointService } from "../../Paypoint/application/PaypointService.js";
+import { PaypointCommand } from '../../Paypoint/infrastructure/PaypointCommand.js';
+import { PaypointCommandActions } from "../../Paypoint/infrastructure/PaypointCommandActions.js";
+import { PaypointComponentActions } from "../../Paypoint/infrastructure/PaypointComponentActions.js";
 
 import { CasualPaymentModel } from "../../CasualPayment/infrastructure/CasualPaymentSchema.js";
 import { CasualPaymentService } from "../../CasualPayment/application/CasualPaymentService.js";
 import { CasualPaymentCommand } from '../../CasualPayment/infrastructure/CasualPaymentCommand.js';
 import { CasualPaymentCommandActions } from "../../CasualPayment/infrastructure/CasualPaymentCommandActions.js";
 
-import { CreditModel } from "../../Credit/infrastructure/CreditSchema.js";
-import { CreditService } from "../../Credit/application/CreditService.js";
-import { CreditCommand } from '../../Credit/infrastructure/CreditCommand.js';
-import { CreditCommandActions } from "../../Credit/infrastructure/CreditCommandActions.js";
+import { CreditModel } from "../../CreditProduct/infrastructure/CreditProductSchema.js";
+import { CreditProductService } from "../../CreditProduct/application/CreditProductService.js";
+import { CreditCommand } from '../../CreditProduct/infrastructure/CreditProductCommand.js';
+import { CreditProductsCommandActions } from "../../CreditProduct/infrastructure/CreditProductCommandActions.js";
 
 import { GuildModel } from "../../Guild/infrastructure/GuildSchema.js";
 import { GuildService } from "../../Guild/application/GuildService.js";
@@ -72,7 +72,28 @@ import { BackupService } from "../../Backup/application/BackupService.js";
 import { BackupCommand } from "../../Backup/infrastructure/BackupCommand.js";
 import { BackupCommandAction } from "../../Backup/infrastructure/BackupCommandAction.js";
 import { RoleProductEventController } from "../../RoleProduct/infrastructure/RoleProductEventController.js";
-import { PaypointEventController } from "../../PaypointRole/infrastructure/PaypointEventController.js";
+import { PaypointEventController } from "../../Paypoint/infrastructure/PaypointEventController.js";
+
+import { CreditChannelLockerService } from "../../CreditChannelLocker/application/CreditChannelLockerService.js";
+import { CreditChannelLockerCommandActions } from "../../CreditChannelLocker/infraestructure/CreditChannelLockerCommandActions.js";
+import { CreditChannelLockerCommand } from "../../CreditChannelLocker/infraestructure/CreditChannelLockerCommand.js";
+import { CreditChannelLockerModel } from "../../CreditChannelLocker/infraestructure/CreditChannelLockSchema.js";
+import { CreditChannelLockerComponentActions } from "../../CreditChannelLocker/infraestructure/CreditChannelLockerComponentActions.js";
+
+import { CreditWalletModel } from "../../CreditWallet/infrastructure/CreditWalletSchema.js";
+import { CreditWalletService } from "../../CreditWallet/application/CreditWalletService.js";
+import { CreditWalletEventController } from "../../CreditWallet/infrastructure/CreditWalletEventController.js";
+import { CreditWalletCommand } from "../../CreditWallet/infrastructure/CreditWalletCommand.js";
+import { CreditWalletCommandActions } from "../../CreditWallet/infrastructure/CreditWalletCommandActions.js";
+
+import { GuildCommandActions } from "../../Guild/infrastructure/GuildCommandActions.js";
+import { RoleProductCommand } from "../../RoleProduct/infrastructure/RoleProductCommand.js";
+import { RoleProductCommandActions } from "../../RoleProduct/infrastructure/RoleProductCommandActions.js";
+import { CreditRewardModel } from "../../CreditReward/infrastructure/CreditRewardSchema.js";
+import { CreditRewardService } from "../../CreditReward/application/CreditRewardService.js";
+import { CreditRewardEventController } from "../../CreditReward/infrastructure/CreditRewardEventController.js";
+import { CreditRewardCommand } from "../../CreditReward/infrastructure/CreditRewardCommand.js";
+import { CreditRewardCommandActions } from "../../CreditReward/infrastructure/CreditRewardCommandActions.js";
 
 await Database.connect()
 
@@ -102,21 +123,32 @@ const roleRepository = new Repository(RoleRecordModel);
 const roleService = new RoleService(roleRepository);
 const roleEventController = new RoleEventController(roleService);
 
+const guildCommandActions = new GuildCommandActions(guildService, roleService, textChannelService);
+GuildCommand.setCallback(guildCommandActions.execute);
+
 const casualPaymentRepository = new Repository(CasualPaymentModel);
 const casualPaymentService = new CasualPaymentService(casualPaymentRepository, guildRepository);
 const casualPaymentCommandAction = new CasualPaymentCommandActions(casualPaymentService);
 CasualPaymentCommand.setCallback(casualPaymentCommandAction.execute);
 
-const creditRepository = new Repository(CreditModel);
-const creditService = new CreditService(creditRepository, guildRepository);
-const creditCommandAction = new CreditCommandActions(creditService);
-CreditCommand.setCallback(creditCommandAction.execute);
+const creditWalletRepository = new Repository(CreditWalletModel);
+const creditWalletService = new CreditWalletService(creditWalletRepository);
+const creditWalletEventController = new CreditWalletEventController(creditWalletService, guildService, memberService);
+const creditWalletCommandActions = new CreditWalletCommandActions(creditWalletService);
+CreditWalletCommand.setCallback(creditWalletCommandActions.execute);
+
+const creditProductRepository = new Repository(CreditModel);
+const creditProductService = new CreditProductService(creditProductRepository, guildRepository);
+const creditProductCommandAction = new CreditProductsCommandActions(creditProductService, guildService);
+CreditCommand.setCallback(creditProductCommandAction.execute);
 
 const paypointRepository = new Repository(PaypointModel);
 
 const roleProductRepository = new Repository(RoleProductModel);
 const roleProductService = new RoleProductService(roleProductRepository);
 const roleProductEventController = new RoleProductEventController(roleProductService);
+const roleProductCommandActions = new RoleProductCommandActions(roleProductService, guildService, roleService);
+RoleProductCommand.setCallback(roleProductCommandActions.execute);
 
 const casualTransactionRepository = new Repository(CasualTransactionModel);
 const casualTransactionService = new CasualTransactionService(casualTransactionRepository);
@@ -124,11 +156,23 @@ const casualTransactionService = new CasualTransactionService(casualTransactionR
 const DMConversactionRepository = new Repository(DMConversactionModel);
 const dmConversactionService = new DMConversactionService(DMConversactionRepository);
 
-const paypointService = new PaypointService(paypointRepository, guildRepository, casualPaymentRepository);
-const paypointCommandAction = new PaypointCommandActions(paypointService, roleService, roleProductService, casualPaymentService);
-const paypointComponentAction = new PaypointComponentActions(paypointService, casualPaymentService, roleProductService, dmConversactionService, memberService);
+const paypointService = new PaypointService(paypointRepository, guildRepository);
+const paypointCommandAction = new PaypointCommandActions(paypointService, guildService, roleProductService, creditProductService, casualPaymentService);
+const paypointComponentAction = new PaypointComponentActions(paypointService, casualPaymentService, roleProductService, creditProductService, dmConversactionService, memberService);
 const paypointEventController = new PaypointEventController(paypointService);
 PaypointCommand.setCallback(paypointCommandAction.execute);
+
+const creditChannelLockerRepository = new Repository(CreditChannelLockerModel);
+const creditChannelLockerService = new CreditChannelLockerService(creditChannelLockerRepository);
+const creditChannelLockerComponentAction = new CreditChannelLockerComponentActions(creditChannelLockerService, creditWalletService, paypointService);
+const creditChannelLockerCommandAction = new CreditChannelLockerCommandActions(creditChannelLockerService);
+CreditChannelLockerCommand.setCallback(creditChannelLockerCommandAction.execute);
+
+const creditRewardRepository = new Repository(CreditRewardModel)
+const creditRewardService = new CreditRewardService(creditRewardRepository)
+const creditRewardEventController = new CreditRewardEventController(creditRewardService, memberService, creditWalletService)
+const creditRewardCommandActions = new CreditRewardCommandActions(creditRewardService, guildService)
+CreditRewardCommand.setCallback(creditRewardCommandActions.execute);
 
 const rewardRoleRespository = new Repository(RewardRoleModel);
 const rewardRoleService = new RewardRoleService(rewardRoleRespository);
@@ -141,11 +185,11 @@ const inviteEventController = new InviteEventController(memberService);
 InviteCommand.setCallback(inviteCommandActions.execute);
 
 const agentEventController = new AgentEventController(casualTransactionService, paypointService, roleProductService, rewardRoleService, dmConversactionService, memberService);
-const agentComponentActions = new AgentComponentsActions(dmConversactionService, casualTransactionService);
+const agentComponentActions = new AgentComponentsActions(dmConversactionService, casualTransactionService, roleProductService, creditProductService, creditWalletService);
 
 const backupRepository = new Repository(BackupModel);
 const backupService = new BackupService(backupRepository);
-const backupCommandAction = new BackupCommandAction(backupService, guildService, roleService, textChannelService, voiceChannelService, categoryChannelService, memberService, roleProductService, rewardRoleService, casualPaymentService, paypointService);
+const backupCommandAction = new BackupCommandAction(backupService, guildService, roleService, textChannelService, voiceChannelService, categoryChannelService, memberService, roleProductService, creditProductService, rewardRoleService, casualPaymentService, paypointService);
 BackupCommand.setCallback(backupCommandAction.execute);
 
 export const Services = {
@@ -156,7 +200,10 @@ export const Services = {
     voiceChannelService,
     roleService,
     casualPaymentService,
-    creditService,
+    creditService: creditProductService,
+    creditChannelLockerService,
+    creditWalletService,
+    creditRewardService,
     paypointService,
     roleProductService,
     rewardRoleService
@@ -173,7 +220,9 @@ export const Controllers = {
     inviteEventController,
     agentEventController,
     roleProductEventController,
-    paypointEventController
+    paypointEventController,
+    creditWalletEventController,
+    creditRewardEventController,
 }
 
 export const Commands: SlashCommandCallable[] = [ 
@@ -185,18 +234,28 @@ export const Commands: SlashCommandCallable[] = [
     InviteCommand, 
     RewardRoleCommand,
     IntegratedPaymentCommand,
-    BackupCommand
+    BackupCommand,
+    CreditChannelLockerCommand,
+    CreditWalletCommand,
+    CreditRewardCommand,
+    RoleProductCommand
 ]
 
 export const CommandActions = {
+    guildCommandActions, 
     paypointCommandAction,
-    creditCommandAction,
+    creditCommandAction: creditProductCommandAction,
+    creditChannelLockerCommandAction,
     rewardRoleCommandAction,
     casualPaymentCommandAction,
-    backupCommandAction
+    backupCommandAction,
+    creditWalletCommandActions,
+    creditRewardCommandActions,
+    RoleProductCommandActions
 }
 
 export const ComponentActions: IComponentAction[] = [
     agentComponentActions,
-    paypointComponentAction
+    paypointComponentAction,
+    creditChannelLockerComponentAction
 ]
