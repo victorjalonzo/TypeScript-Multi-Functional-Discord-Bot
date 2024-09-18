0131c64081cd45c2089d9d80e75fc798005502ca
VJA <victorj.alonzo@gmail.com>
2024-09-18 09:07:53 -0400

    Added subcommand for default settings.

--- src/Guild/infrastructure/GuildCommand.ts
+++ src/Guild/infrastructure/GuildCommand.ts
@@ -1,60 +1,68 @@
+import { ChannelType } from "discord.js";
 import { SlashCommandCallable } from "../../shared/intraestructure/SlashCommandCallable.js";
-import { GuildActions } from "./GuildActions.js";
 
 const GuildCommand = new SlashCommandCallable()
-const actions = new GuildActions()
 
 GuildCommand
     .setName('guild')
     .setDescription('Commands for guilds management')
-    .setCallback(actions.execute)
+
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('show-information')
+            .setDescription('Show the guild configuration and information.')
+    )
     
     .addSubcommand(subcommand =>
         subcommand
-            .setName('create')
-            .setDescription('Create a new guild')
-
-            .addStringOption(option => option
-                .setName('name')
-                .setDescription('The name of the guild')
-                .setRequired(true)
-            )
-            .addStringOption(option => option
-                .setName('template')
-                .setDescription('The name of the template to use for the guild')
-                .setRequired(true)
-            )
-    )
-
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('delete')
-            .setDescription('Delete a guild by its ID')
+            .setName('set-default-credits')
+            .setDescription('Set the default credits amount of all the members of a guild')
 
             .addIntegerOption(option => option
-                .setName('id')
-                .setDescription('The id of the guild')
+                .setName('credits')
+                .setDescription('The amount of credits')
                 .setRequired(true)
             )
-    )
-            
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('list')
-            .setDescription('Show the list of available guilds')
     )
 
     .addSubcommand(subcommand =>
         subcommand
-            .setName('link')
-            .setDescription('Get the link of a guild')
+            .setName('set-default-role')
+            .setDescription('Set the default role of all the members of a guild')
 
-            .addIntegerOption(option => option
-                .setName('id')
-                .setDescription('The id of the guild')
+            .addRoleOption(option => option
+                .setName('role')
+                .setDescription('The default role')
                 .setRequired(true)
             )
-    );
+    )
+
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('set-default-notification-channel')
+            .setDescription('Set the default notification channel of the guild')
+
+            .addChannelOption(option => option
+                .setName('channel')
+                .setDescription('The default notification channel')
+                .setRequired(true)
+                .addChannelTypes(ChannelType.GuildText)
+            )
+    )
+
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('set-default-invoice-channel')
+            .setDescription('Set the default invoice channel of the guild')
+
+            .addChannelOption(option => option
+                .setName('channel')
+                .setDescription('The default invoice channel')
+                .setRequired(true)
+                .addChannelTypes(ChannelType.GuildText)
+            )
+    )
+
 
 GuildCommand.setDefaultMemberPermissions(8)
 GuildCommand.setDMPermission(true)
