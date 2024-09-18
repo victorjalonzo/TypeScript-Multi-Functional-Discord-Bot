20e0571c48d41268a6deb8e870c4a100a8ea8fdf
VJA <victorj.alonzo@gmail.com>
2024-09-17 22:53:59 -0400

    Added field name to the interface.

--- src/CreditProduct/domain/ICreditProduct.ts
+++ src/CreditProduct/domain/ICreditProduct.ts
@@ -2,10 +2,11 @@ import {IGuild} from "../../Guild/domain/IGuild.js";
 
 export interface ICreditProduct {
     id: string
+    name: string
     price: number
-    amount: number
+    credits: number
     media?: Buffer | null
-    codec?: string | null
+    mediaFilename?: string | null
     description?: string | null
     guild: IGuild
     guildId: string
