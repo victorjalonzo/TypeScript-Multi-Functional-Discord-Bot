a215c702b88d6f87c954353763e67dd7dba37e44
VJA <victorj.alonzo@gmail.com>
2024-09-17 23:09:29 -0400

    Added id as field and property.

--- src/CasualPayment/domain/CasualPayment.ts
+++ src/CasualPayment/domain/CasualPayment.ts
@@ -1,15 +1,17 @@
 import { ICasualPayment } from "./ICasualPayment.js";
 import { TPaymentMethods } from "./ICasualPayment.js";
 import { IGuild } from "../../Guild/domain/IGuild.js";
+import { createRandomId } from "../../shared/utils/generate.js";
 
 export class CasualPayment implements ICasualPayment {
-    name: TPaymentMethods
-    value: string
-    guildId: string
-    guild: IGuild
-    rawName: string
+    public id: string = createRandomId()
+    public name: TPaymentMethods
+    public value: string
+    public guildId: string
+    public guild: IGuild
+    public rawName: string
 
-    constructor(options: Omit<ICasualPayment, "rawName">){
+    constructor(options: Omit<ICasualPayment, "id"| "rawName">){
         this.name = options.name
         this.value = options.value
         this.guildId = options.guildId
--- src/CasualPayment/domain/ICasualPayment.ts
+++ src/CasualPayment/domain/ICasualPayment.ts
@@ -4,6 +4,7 @@ import { ICachedGuild } from "../../shared/intraestructure/ICachedGuild.js";
 export type TPaymentMethods = "Cash App" | "Zelle" | "Paypal" | "Venmo" | "Apple Pay" | "Google Pay";
 
 export interface ICasualPayment {
+    id: string
     name: TPaymentMethods
     value: string
     guildId: string
