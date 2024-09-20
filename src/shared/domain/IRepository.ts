e3c924fc082b6d1edcad77ff5d941a063ef36b2a
VJA <victorj.alonzo@gmail.com>
2024-09-19 23:34:06 -0400

    Added the populate option type string or string[] to the methods delete() and deleteAll()

--- src/shared/domain/IRepository.ts
+++ src/shared/domain/IRepository.ts
@@ -1,8 +1,8 @@
 export interface IRepository <T> {
     create(data: Record<string, any>): Promise<T>
     update(filters: Record<string, any>, data: Record<string, any>): Promise<T | null>
-    delete(filters: Record<string, any>): Promise<T | null>
-    deleteAll(filters: Record<string, any>): Promise<T[]>
+    delete(filters: Record<string, any>, populate?: string[] | string): Promise<T | null>
+    deleteAll(filters: Record<string, any>, populate?: string[] | string): Promise<T[]>
     get(filters?: Record<string, any>, populate?: string[] | string): Promise<T | null>
     getAll(filters?: Record<string, any>, populate?: string[] | string): Promise<T[]>
 }
