4c5590f96457e4000340bc2c2ddf9ffa7e6bb635
VJA <victorj.alonzo@gmail.com>
2024-09-20 23:26:43 -0400

    Removed the method 'getByRawName' from the interface.

--- src/CasualPayment/domain/ICasualPaymentInput.ts
+++ src/CasualPayment/domain/ICasualPaymentInput.ts
@@ -3,8 +3,7 @@ import { Result } from "../../shared/domain/Result.js";
 
 export interface ICasualPaymentInput {
     create(casualPayment: ICasualPayment): Promise<Result<ICasualPayment>>
-    get ( filters: Record<string, any> ): Promise<Result<ICasualPayment>>
-    getByRawName(rawName: string, guildId: string): Promise<Result<ICasualPayment>>
+    get (id: string, guildId: string): Promise<Result<ICasualPayment>>
     getAll(guildId: string): Promise<Result<ICasualPayment[]>>
     delete({name, guildId }: { name: string, guildId: string }): Promise<Result<Record<string, any>>>
     deleteAll(guildId: string): Promise<Result<ICasualPayment[]>>
