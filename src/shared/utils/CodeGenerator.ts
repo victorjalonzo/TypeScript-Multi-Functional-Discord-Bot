5f156ddf1df65fe93d234d1a6cd4a409580a0090
VJA <victorj.alonzo@gmail.com>
2024-09-21 23:51:20 -0400

    Added a code generator

---
+++ src/shared/utils/CodeGenerator.ts
@@ -0,0 +1,8 @@
+export const generateCode = (length: number): string => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomCode = '';
+    for (let i = 0; i < length; i++) {
+        randomCode += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return randomCode;
+}
