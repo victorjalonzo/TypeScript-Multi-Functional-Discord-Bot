702fe455b965d7809264f2909f9294b38cd539f4
VJA <victorj.alonzo@gmail.com>
2024-09-22 23:42:03 -0400

    Changed params names.

--- src/Role/domain/IRoleInput.ts
+++ src/Role/domain/IRoleInput.ts
@@ -4,7 +4,7 @@ import { Result } from "../../shared/domain/Result.js"
 export interface IRoleInput {
     create (role: IRole): Promise<Result<IRole>>
     update (role: IRole): Promise<Result<IRole>>
-    get (roleId: string, guildId: string): Promise<Result<IRole>>
+    get (id: string, guildId: string): Promise<Result<IRole>>
     getAll (guildId: string): Promise<Result<IRole[]>>
-    delete (roleId: string, guildId: string): Promise<Result<IRole>>
+    delete (id: string, guildId: string): Promise<Result<IRole>>
 }
