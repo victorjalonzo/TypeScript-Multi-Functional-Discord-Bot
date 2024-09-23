33f1057458fd01889a6880e30b7f9fae325ba916
VJA <victorj.alonzo@gmail.com>
2024-09-22 23:50:41 -0400

    Added a refresh method and modified createRecord, updateRecord and deleteRecord

--- src/TextChannel/infrastructure/TextChannelEventController.ts
+++ src/TextChannel/infrastructure/TextChannelEventController.ts
@@ -1,19 +1,128 @@
 import { ITextChannelInput } from "../domain/ITextChannelInput.js";
-import { TextChannel as DiscordTextChannel } from "discord.js";
+import { TextChannel as DiscordTextChannel, Guild as DiscordGuild, ChannelType } from "discord.js";
 import { TextChannelTransformer } from "./TextChannelTransformer.js";
 import { logger } from "../../shared/utils/logger.js";
 import { GuildTransformer } from "../../Guild/infrastructure/GuildTransformer.js";
 import { CategoryChannelTransformer } from "../../CategoryChannel/infrastructure/CategoryChannelTransformer.js";
+import { IGuildInput } from "../../Guild/domain/IGuildInput.js";
+import { IGuild } from "../../Guild/domain/IGuild.js";
+import { ITextChannel } from "../domain/ITextChannel.js";
+import { Result } from "../../shared/domain/Result.js";
+import { ICategoryChannelInput } from "../../CategoryChannel/domain/ICategoryChannelInput.js";
+import { ICategoryChannel } from "../../CategoryChannel/domain/ICategoryChannel.js";
+import { printTextChannelRefreshStatus } from "../domain/TextChannelFreshStatus.js";
 
 export class TextChannelEventController {
     constructor (
-        private service: ITextChannelInput
+        private service: ITextChannelInput,
+        private guildService: IGuildInput,
+        private categoryChannelService: ICategoryChannelInput
     ) {}
+
+    refresh = async (guild: DiscordGuild): Promise<void> => {
+        try {
+            const channelsCreated: ITextChannel[] = [];
+            const channelsUpdated: ITextChannel[] = [];
+            const channelsDeleted: ITextChannel[] = [];
+
+            const [guildCachedResult, channelsCachedResult] = await Promise.all([
+                this.guildService.get(guild.id),
+                this.service.getAll(guild.id)
+            ]);
+
+            if (!guildCachedResult.isSuccess() || !channelsCachedResult.isSuccess()) {
+                throw guildCachedResult.error || channelsCachedResult.error;
+            }
+
+            const guildCached = guildCachedResult.value as IGuild;
+            const channelsCached = channelsCachedResult.value as ITextChannel[];
+
+            let channels: DiscordTextChannel[];
+
+            try {
+                channels = (await guild.channels.fetch())
+                    .filter(channel => channel !== null && channel.type === ChannelType.GuildText)
+                    .map(channel => channel as DiscordTextChannel);
+            } catch (e) {
+                throw new Error(`Error fetching text channels: ${String(e)}`);
+            }
+
+            if (channels.length === 0) {
+                return printTextChannelRefreshStatus({
+                    channelsCreated: channelsCreated.length, 
+                    channelsUpdated: channelsUpdated.length, 
+                    channelsDeleted: channelsDeleted.length
+                });
+            }
+
+            const channelsRefreshed: ITextChannel[] = [];
+
+            for (const channel of channels) {
+                const match = channelsCached.find(c => c.id === channel.id);
+
+                let categoryChannelCached: ICategoryChannel | undefined;
+
+                if (channel.parentId !== null) {
+                    const categoryChannelResult = await this.categoryChannelService.get(channel.parentId, guild.id);
+                    if (!categoryChannelResult.isSuccess()) categoryChannelCached = categoryChannelResult.value;
+                }
+
+                const channelParsed: ITextChannel = TextChannelTransformer.parse({
+                    discordTextChannel: channel,
+                    parent: categoryChannelCached ?? null,
+                    guild: guildCached
+                });
+
+                let result: Result<ITextChannel>;
+
+                if (match) {
+                    result = await this.service.update(channelParsed);
+                    if (!result.isSuccess()) throw result.error;
+
+                    channelsUpdated.push(result.value);
+                } else {
+                    result = await this.service.create(channelParsed);
+                    if (!result.isSuccess()) throw result.error;
+
+                    channelsCreated.push(result.value);
+                }
+
+                channelsRefreshed.push(result.value);
+            }
+
+            const channelObsoletes = channelsCached.filter(channelCached => {
+                return !channelsRefreshed.some(channelRefreshed => channelRefreshed.id === channelCached.id);
+            });
+
+            for (const channelObsolete of channelObsoletes) {
+                const result = await this.service.delete(channelObsolete.id, guild.id);
+                if (!result.isSuccess()) throw result.error;
+
+                channelsDeleted.push(channelObsolete);
+            }
+
+            printTextChannelRefreshStatus({
+                channelsCreated: channelsCreated.length, 
+                channelsUpdated: channelsUpdated.length, 
+                channelsDeleted: channelsDeleted.length
+            });
+        }
+        catch (e) {
+            logger.warn(e);
+        }
+    }
 
     createRecord = async (textChannel: DiscordTextChannel): Promise<void> => {
         try {
+            const guildCachedResult = await this.guildService.get(textChannel.guildId);
+            if (!guildCachedResult.isSuccess()) throw guildCachedResult.error
+
+            const guildCached = guildCachedResult.value as IGuild
+
             const guild = GuildTransformer.parse(textChannel.guild)
-            const parent = textChannel.parent ? CategoryChannelTransformer.parse(textChannel.parent) : null
+            const parent = textChannel.parent 
+                ? CategoryChannelTransformer.parse(textChannel.parent, guildCached) 
+                : null
 
             const textChannelParsed = TextChannelTransformer.parse({discordTextChannel: textChannel, parent, guild});
             
@@ -22,7 +131,7 @@ export class TextChannelEventController {
     
             const textChannelCreated = result.value
     
-            logger.info(`The text channel ${textChannelCreated.name} (${textChannelCreated.id}) was created`)
+            logger.info(`Text channel ${textChannelCreated.name} (${textChannelCreated.id}) was created`)
         }
         catch (e) {
             logger.warn(e)
@@ -31,8 +140,15 @@ export class TextChannelEventController {
 
     updateRecord = async (oldTextChannel: DiscordTextChannel, newTextChannel: DiscordTextChannel): Promise<void> => {
         try {
+            const guildCachedResult = await this.guildService.get(newTextChannel.guildId);
+            if (!guildCachedResult.isSuccess()) throw guildCachedResult.error
+
+            const guildCached = guildCachedResult.value as IGuild
+
             const guild = GuildTransformer.parse(newTextChannel.guild)
-            const parent = newTextChannel.parent ? CategoryChannelTransformer.parse(newTextChannel.parent) : null
+            const parent = newTextChannel.parent 
+                ? CategoryChannelTransformer.parse(newTextChannel.parent, guildCached) 
+                : null
 
             const textChannelParsed = TextChannelTransformer.parse({discordTextChannel: newTextChannel,parent, guild});
             
@@ -40,7 +156,7 @@ export class TextChannelEventController {
             if (!result.isSuccess()) throw result.error
 
             const textChannelUpdated = result.value
-            logger.info(`The text channel ${textChannelUpdated.name} (${textChannelUpdated.id}) was updated`)
+            logger.info(`Text channel ${textChannelUpdated.name} (${textChannelUpdated.id}) was updated`)
         }
         catch (e) {
             logger.warn(e)
@@ -55,7 +171,7 @@ export class TextChannelEventController {
             if (!result.isSuccess()) throw result.error
     
             const textChannelDeleted = result.value
-            logger.info(`The text channel ${textChannelDeleted.name} (${textChannelDeleted.id}) was deleted`)
+            logger.info(`Text channel ${textChannelDeleted.name} (${textChannelDeleted.id}) was deleted`)
         }
         catch (e) {
             logger.warn(e)
