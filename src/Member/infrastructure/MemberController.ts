8702c62bad6866e21a0c21f106df5eca3f8102ec
VJA <victorj.alonzo@gmail.com>
2024-09-22 23:53:39 -0400

    Added a method refresh.

--- src/Member/infrastructure/MemberController.ts
+++ src/Member/infrastructure/MemberController.ts
@@ -1,19 +1,64 @@
 import { IMemberInput } from "../domain/IMemberInput.js";
-import { GuildMember } from "discord.js";
+import { Guild as DiscordGuild, GuildMember } from "discord.js";
 import { MemberTransformer } from "../infrastructure/MemberTransformer.js";
 import { logger } from "../../shared/utils/logger.js";
-import { cache } from "../../shared/intraestructure/Cache.js";
+import { IGuildInput } from "../../Guild/domain/IGuildInput.js";
 
 export class MemberController {
-    constructor(private service: IMemberInput) {}
+    constructor(
+        private service: IMemberInput,
+        private guildService: IGuildInput
+    ) {}
+
+    async refresh (guild: DiscordGuild): Promise<void> {
+        try {
+            const guildCachedResult = await this.guildService.get(guild.id)
+            if (!guildCachedResult.isSuccess()) throw guildCachedResult.error
+
+            const guildCached = guildCachedResult.value
+
+            const guildMembers = (await guild.members.fetch()).toJSON();
+
+            const membersStoredResult = await this.service.getAll(guild.id)
+            if (!membersStoredResult.isSuccess()) throw membersStoredResult.error
+
+            const membersStored = membersStoredResult.value
+
+            const guildMembersNotStored = guildMembers.filter(guildMember => {
+                return !membersStored.some(member => member.id === guildMember.id)
+            })
+
+            if (guildMembersNotStored.length === 0) {
+                logger.info("members: up to date.")
+                return
+            }
+
+            for (const guildMember of guildMembersNotStored) {
+                const member = MemberTransformer.parse(guildMember, guildCached)
+
+                const memberCreatedResult = await this.service.create(member)
+                if (!memberCreatedResult.isSuccess()) throw memberCreatedResult.error
+
+                logger.info(`The member ${member.username} (${member.id}) was created`)
+            }
+
+            logger.info(`The members of ${guild.name} (${guild.id}) were refreshed.\nTotal members stored after refresh: ${guildMembersNotStored.length}`)
+        }
+        catch (e) {
+            logger.warn(e)
+        }
+    }
 
     async create(member: GuildMember): Promise<void> {
         try {
-            const cachedGuild = cache.get(member.guild.id)
+            const guildCachedResult = await this.guildService.get(member.guild.id)
+            if (!guildCachedResult.isSuccess()) throw guildCachedResult.error
 
-            if (!cachedGuild) throw new Error(`The guild ${member.guild.id} was not cached`)
+            const guildCached = guildCachedResult.value
 
-            const parsedMember = MemberTransformer.parse(member, cachedGuild)
+            if (!guildCached) throw new Error(`The guild ${member.guild.id} was not cached`)
+
+            const parsedMember = MemberTransformer.parse(member, guildCached)
             const resul = await this.service.create(parsedMember)
 
             if (!resul.isSuccess()) throw resul.error
@@ -27,11 +72,14 @@ export class MemberController {
     }
 
     async update(oldMember: GuildMember, newMember: GuildMember): Promise<void> {
-        const cachedGuild = cache.get(oldMember.guild.id)
+            const guildCachedResult = await this.guildService.get(oldMember.guild.id)
+            if (!guildCachedResult.isSuccess()) throw guildCachedResult.error
+            
+            const guildCached = guildCachedResult.value
 
-        if (!cachedGuild) throw new Error(`The guild ${oldMember.guild.id} was not cached`)
+        if (!guildCached) throw new Error(`The guild ${oldMember.guild.id} was not cached`)
 
-        const newParsedMember = MemberTransformer.parse(newMember, cachedGuild)
+        const newParsedMember = MemberTransformer.parse(newMember, guildCached)
 
         try {
             const result = await this.service.update(newParsedMember)
