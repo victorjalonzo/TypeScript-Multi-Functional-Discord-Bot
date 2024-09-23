5ac0adaf16682c279d5fd58d199c4ea39b38d9b9
VJA <victorj.alonzo@gmail.com>
2024-09-22 23:53:07 -0400

    Updated method parse to accept a guild as parameter.

--- src/Member/infrastructure/MemberTransformer.ts
+++ src/Member/infrastructure/MemberTransformer.ts
@@ -1,9 +1,10 @@
 import {GuildMember} from "discord.js";
 import { Member } from "../domain/Member.js";
 import { ICachedGuild } from "../../shared/intraestructure/ICachedGuild.js";
+import { IGuild } from "../../Guild/domain/IGuild.js";
 
 export class MemberTransformer {
-    static parse = (guildMember: GuildMember, guild: ICachedGuild): Member => {
+    static parse = (guildMember: GuildMember, guild: IGuild): Member => {
         try {
             return new Member({
                 id: guildMember.id,
