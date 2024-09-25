1bd7683d028a8c8f3e5ed379a6e7eb4dff58c070
VJA <victorj.alonzo@gmail.com>
2024-09-24 23:51:14 -0400

    implemented the method setInvitedBy in the class.

--- src/Member/domain/Member.ts
+++ src/Member/domain/Member.ts
@@ -1,22 +1,31 @@
 import { IMember } from "./IMember.js";
 import { IGuild } from "../../Guild/domain/IGuild.js";
 
+interface IProps extends Omit<IMember, "guildId" | "invitedById" | "setInvitedBy"> {}
+
 export class Member implements IMember {
     id: string
     username: string
     discriminator: string
     guildId: string
     guild: IGuild
-    avatarURL?: string | null
-    invitedBy?: string | null
+    avatarURL?: string
+    invitedById?: string
+    invitedBy?: IMember
 
-    constructor (props: IMember){
+    constructor (props: IProps){
         this.id = props.id
         this.username = props.username
         this.discriminator = props.discriminator
-        this.guildId = props.guildId
         this.guild = props.guild
+        this.guildId = props.guild.id
         this.avatarURL = props.avatarURL
         this.invitedBy = props.invitedBy
+        this.invitedById = props.invitedBy?.id
+    }
+
+    setInvitedBy(inviter: IMember): void {
+        this.invitedBy = inviter
+        this.invitedById = inviter.id
     }
 }
