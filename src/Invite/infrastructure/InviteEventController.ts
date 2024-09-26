f11eaf0f885eaa0f02672785d54648296fd15f43
VJA <victorj.alonzo@gmail.com>
2024-09-25 23:36:53 -0400

    Updated the method increaseInviteCount.

--- src/Invite/infrastructure/InviteEventController.ts
+++ src/Invite/infrastructure/InviteEventController.ts
@@ -9,54 +9,63 @@ import {
     InviterNotFoundError
 }
 from "../domain/inviteExceptions.js"
+import { ICachedGuild } from "../../shared/intraestructure/ICachedGuild.js";
 
 export class InviteEventController {
     constructor (private memberService: IMemberInput) {}
 
     increaseInviteCount = async (member: GuildMember) => {
         try {
-            const guild = member.guild;
-
-            const cachedGuild = cache.get(guild.id)
-            if (!cachedGuild) throw new Error(`The guild ${member.guild.id} was not cached`)
+            //Get the member record
+            const memberRecordResult = await this.memberService.get(member.id, member.guild.id)
+            if (!memberRecordResult.isSuccess()) throw memberRecordResult.error
+            const memberRecord = memberRecordResult.value
+            
+            //If the member already has an inviter, end the function
+            if (memberRecord.invitedBy) throw new DuplicateUserInviteError()
     
-            const inviteManager = guild.invites
-    
+            //Get the old invite data from the cached guild and the new one
+            const guildCached = <ICachedGuild>cache.get(member.guild.id)        
+            const newInviteData = await member.guild.invites.fetch()
+            const oldInviteData = <Collection<string, Invite>>guildCached.inviteData
+            
+            //Find the inviter by the invite code updated in the new invite data
             let inviter: User | null | undefined = undefined
-    
-            const newInviteData = await inviteManager.fetch()
-            const oldInviteData = <Collection<string, Invite>>cachedGuild.inviteData
     
             for (const [code, oldInvite] of oldInviteData.entries()) {
                 const newInvite = newInviteData.get(code)
                 if (!newInvite) continue
 
-                if (!newInvite.uses) continue
-                if (!oldInvite.uses) continue
-    
+                if (!newInvite.uses || !oldInvite.uses) continue
                 if (newInvite.uses <= oldInvite.uses) continue
     
                 inviter = newInvite.inviter
                 if (!inviter) break
             }
     
+            //If there is no inviter, end the function
             if (inviter === undefined) throw new UserJoinedWithoutInviterError()
             if (inviter === null) throw new InviterNotFoundError()
+
+            //Get the inviter record
+            const inviterRecordResult = await this.memberService.get(inviter.id, member.guild.id)
+            if (!inviterRecordResult.isSuccess()) throw inviterRecordResult.error
+            const inviterRecord = inviterRecordResult.value
     
-            const memberResult = await this.memberService.get(member.id, guild.id)
-            if (!memberResult.isSuccess()) throw memberResult.error
+            //Update the member record object with the inviter data
+            memberRecord.invitedBy = inviterRecord
+            memberRecord.invitedById = inviter.id
     
-            const memberRecord = memberResult.value
-            if (memberRecord.invitedBy) throw new DuplicateUserInviteError()
-    
-            memberRecord.invitedBy = inviter.id
-    
-            await this.memberService.update(memberRecord)
+            //Update the member record
+            const memberRecordUpdateResult = await this.memberService.update(memberRecord)
+            if (!memberRecordUpdateResult.isSuccess()) throw memberRecordUpdateResult.error
     
             logger.info(`The user ${member.user.username} (${member.user.id}) joined with an invitation from ${inviter.username} (${inviter.id})`)
         }
         catch (e) {
-            logger.warn(e)
+            if (e instanceof DuplicateUserInviteError) {
+                logger.info(`The user ${member.user.username} (${member.user.id}) joined without an invitation`)
+            }
         }
 
     }
