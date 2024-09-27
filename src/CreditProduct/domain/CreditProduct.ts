236cd3c5de8833fa0d2a31e88ae3b4bdeaa0d891
VJA <victorj.alonzo@gmail.com>
2024-09-26 23:31:40 -0400

    Added type property.

--- src/CreditProduct/domain/CreditProduct.ts
+++ src/CreditProduct/domain/CreditProduct.ts
@@ -1,6 +1,9 @@
 import { ICreditProduct } from "./ICreditProduct.js";
 import { IGuild } from "../../Guild/domain/IGuild.js";
 import { createRandomId } from "../../shared/utils/IDGenerator.js";
+import { ProductType } from "../../shared/domain/ProductTypeEnums.js";
+
+interface IProps extends Omit<ICreditProduct, "id" | "name" | "type" | "createdAt">{}
 
 export class CreditProduct implements ICreditProduct {
     public id: string = createRandomId()
@@ -10,11 +13,13 @@ export class CreditProduct implements ICreditProduct {
     public media?: Buffer | null
     public mediaFilename?: string | null
     public description?: string | null
+    public type: ProductType = ProductType.CREDIT
     public guildId: string
     public guild: IGuild
     public createdAt: Date = new Date()
 
-    constructor(props: Omit<ICreditProduct, "id" | "name" | "createdAt">) {
+    constructor(props: IProps) {
+        this.name = `${props.credits} credits`
         this.price = props.price
         this.credits = props.credits
         this.media = props.media
@@ -22,7 +27,5 @@ export class CreditProduct implements ICreditProduct {
         this.description = props.description
         this.guildId = props.guildId
         this.guild = props.guild
-
-        this.name = `${props.credits} Credits`
     }
 }
