8d5c592e8784bf53f507e05f0b67942302049d53
VJA <victorj.alonzo@gmail.com>
2024-09-26 23:33:03 -0400

    Added type field

--- src/RoleProduct/domain/IRoleProduct.ts
+++ src/RoleProduct/domain/IRoleProduct.ts
@@ -1,13 +1,17 @@
 import { IRole } from '../../Role/domain/IRole.js';
 import { IGuild } from '../../Guild/domain/IGuild.js';
+import { ProductType } from '../../shared/domain/ProductTypeEnums.js';
 
 export interface IRoleProduct {
     id: string
+    name: string
     role: IRole
     price: number
     media?: Buffer | null
     mediaFilename?: string | null
     description?: string | null
+    type: ProductType
     guild: IGuild
     guildId: string
+    createdAt: Date
 }
