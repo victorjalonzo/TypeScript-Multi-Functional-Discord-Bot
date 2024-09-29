de8f0b5a2b094ca243036cd627563f50cb3633d7
VJA <victorj.alonzo@gmail.com>
2024-09-28 23:23:16 -0400

    Added a method to get active thread conversation by its thread channel

--- src/ThreadConversaction/domain/IThreadConversationInput.ts
+++ src/ThreadConversaction/domain/IThreadConversationInput.ts
@@ -5,9 +5,9 @@ export interface IThreadConversationInput {
     create (DMConversaction: IThreadConversation): Promise<Result<IThreadConversation>>
     get(id: string): Promise<Result<IThreadConversation>>
     getByMember(memberId: string): Promise<Result<IThreadConversation>>
+    getActiveOneByThreadChannel(threadChannelId: string, guildId: string): Promise<Result<IThreadConversation>>
     getActiveOneByMember(memberId: string): Promise<Result<IThreadConversation>>
     update(DMConversaction: IThreadConversation): Promise<Result<IThreadConversation>>
     delete(memberId: string): Promise<Result<IThreadConversation>>
     isAwaitingAdminApproval(memberId: string): Promise<Result<boolean>>
-    isAwaitingMemberApproval(memberId: string): Promise<Result<boolean>>
 }
