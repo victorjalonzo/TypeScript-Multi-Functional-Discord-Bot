0ee528fe8b8675fa109aa9302c1e114ee9271bad
VJA <victorj.alonzo@gmail.com>
2024-09-29 23:52:05 -0400

    Added the method isPending to the schema.

--- src/CasualTransaction/infrastructure/CasualTransactionSchema.ts
+++ src/CasualTransaction/infrastructure/CasualTransactionSchema.ts
@@ -1,15 +1,17 @@
 import { Document, model, Schema} from "mongoose"
 import { ICasualTransaction } from "../domain/ICasualTransaction.js"
+import { CasualTransactionState } from "../domain/CasualTransactionStateEnums.js"
 
 const CasualTransactionSchema = new Schema<Document & ICasualTransaction>({
     id: { type: String, required: true, unique: true },
     member: { type: Schema.Types.ObjectId, ref: "Members", required: true },
     memberId: { type: String, required: true },
+    guild: { type: Schema.Types.ObjectId, ref: "Guilds", required: true },
     guildId: { type: String, required: true },
     state: { type: Number, required: true},
-    paymentMethodId: { type: String, default: null },
-    paymentMethodName: { type: String, default: null },
-    paymentMethodValue: { type: String, default: null },
+    casualPaymentMethodId: { type: String, default: null },
+    casualPaymentMethodName: { type: String, default: null },
+    casualPaymentMethodValue: { type: String, default: null },
     productId: { type: String, default: null },
     productName: { type: String, default: null },
     productPrice: { type: Number, default: null },
@@ -19,5 +21,9 @@ const CasualTransactionSchema = new Schema<Document & ICasualTransaction>({
     createAt: { type: Date, required: true },
     expiredAt: { type: Date, default: null }
 })
+
+CasualTransactionSchema.methods.isPending = function() {
+    return this.state === CasualTransactionState.PENDING
+}
 
 export const CasualTransactionModel = model<Document & ICasualTransaction>('CasualTransactions', CasualTransactionSchema)
