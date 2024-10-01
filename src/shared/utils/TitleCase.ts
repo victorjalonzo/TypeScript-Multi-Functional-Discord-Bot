689823fa5440dcaac98325ef078a356fbcf505e0
VJA <victorj.alonzo@gmail.com>
2024-09-30 23:16:11 -0400

    Added utility for title case formatting.

---
+++ src/shared/utils/TitleCase.ts
@@ -0,0 +1,7 @@
+export const TitleCase = (text: string): string => {
+    return text
+        .toLowerCase()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+}
