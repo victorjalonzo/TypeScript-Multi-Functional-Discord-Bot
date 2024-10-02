d524519e57bcf70af960bb13695c5afc0a15493c
VJA <victorj.alonzo@gmail.com>
2024-10-02 09:04:47 -0400

    Update field name

--- src/CasualPayment/domain/ICasualPayment.ts
+++ src/CasualPayment/domain/ICasualPayment.ts
@@ -1,11 +1,10 @@
 import { IGuild } from "../../Guild/domain/IGuild.js";
 import { ICachedGuild } from "../../shared/intraestructure/ICachedGuild.js";
-
-export type TPaymentMethods = "Cash App" | "Zelle" | "Paypal" | "Venmo" | "Apple Pay" | "Google Pay";
+import { CasualPaymentMethodType } from "./CasualPaymentMethodType.js";
 
 export interface ICasualPayment {
     id: string
-    name: TPaymentMethods
+    name: CasualPaymentMethodType
     value: string
     guildId: string
     guild: IGuild | ICachedGuild
