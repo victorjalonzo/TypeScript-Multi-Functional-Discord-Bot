1eb1ce8a9ce646fbc0ea029ac9d2b06194104bf8
VJA <victorj.alonzo@gmail.com>
2024-10-01 23:10:02 -0400

    Added field to track the credits rewards completed.

--- src/CreditWallet/domain/ICreditWallet.ts
+++ src/CreditWallet/domain/ICreditWallet.ts
@@ -1,3 +1,4 @@
+import { ICreditReward } from "../../CreditReward/domain/ICreditReward.js"
 import { IGuild } from "../../Guild/domain/IGuild.js"
 import { IMember } from "../../Member/domain/IMember.js"
 
@@ -5,6 +6,7 @@ export interface ICreditWallet {
     memberId: string
     member: IMember
     credits: number
+    creditRewardChallengesCompleted: ICreditReward[]
     guild: IGuild
     guildId: string
 }
