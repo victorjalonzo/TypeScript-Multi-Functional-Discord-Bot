bf0b0efc3f781cfa31dfa3c4733c05ae71256c9b
VJA <victorj.alonzo@gmail.com>
2024-10-02 09:03:47 -0400

    Added methods type

---
+++ src/CasualPayment/domain/CasualPaymentMethodType.ts
@@ -0,0 +1,10 @@
+export enum CasualPaymentMethodType {
+    CashApp = 'Cash App',
+    Zelle = 'Zelle',
+    Paypal = 'PayPal',
+    Venmo = 'Venmo',
+    ApplePay = 'Apple Pay',
+    GooglePay = 'Google Pay',
+    Bitcoin = 'Bitcoin',
+    Ethereum = 'Ethereum'
+}
