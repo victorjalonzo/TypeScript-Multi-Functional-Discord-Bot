3da32299fb2a49668dbbf105741cb816204910cf
VJA <victorj.alonzo@gmail.com>
2024-10-01 23:10:27 -0400

    Added property to track the credit rewards completed.

--- src/CreditWallet/domain/CreditWallet.ts
+++ src/CreditWallet/domain/CreditWallet.ts
@@ -1,18 +1,24 @@
+import { ICreditReward } from "../../CreditReward/domain/ICreditReward.js"
 import { IGuild } from "../../Guild/domain/IGuild.js"
 import { IMember } from "../../Member/domain/IMember.js"
+import { ICreditWallet } from "./ICreditWallet.js"
 
-export class CreditWallet {
+interface IProps extends Omit<ICreditWallet, "guildId" | "memberId" |"creditRewardChallengesCompleted"> {}
+
+export class CreditWallet implements ICreditWallet {
     memberId: string
     member: IMember
     credits: number
+    creditRewardChallengesCompleted: ICreditReward[]
     guild: IGuild
     guildId: string
 
-    constructor (props: CreditWallet) {
-        this.memberId = props.memberId
+    constructor (props: IProps) {
         this.member = props.member
-        this.credits = props.credits
         this.guild = props.guild
-        this.guildId = props.guildId
+        this.credits = props.credits
+        this.memberId = props.member.id
+        this.guildId = props.guild.id
+        this.creditRewardChallengesCompleted = []
     }
 }
