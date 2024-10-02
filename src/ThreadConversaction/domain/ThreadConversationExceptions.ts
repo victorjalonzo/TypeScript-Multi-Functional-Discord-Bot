fd7ae683887264c6ee0dd6c57cae38f4016470ed
VJA <victorj.alonzo@gmail.com>
2024-10-01 23:06:11 -0400

    Added exceptions for states.

--- src/ThreadConversaction/domain/ThreadConversationExceptions.ts
+++ src/ThreadConversaction/domain/ThreadConversationExceptions.ts
@@ -1,29 +1,46 @@
 export class ThreadConversationCreationError extends Error {
     constructor() {
-        super('The DM conversaction could not be created');
+        super('Thread conversaction could not be created');
     }
 }
 
 export class ThreadConversationUpdateError extends Error {
     constructor() {
-        super('The DM conversaction could not be updated');
+        super('Thread conversaction could not be updated');
     }
 }
 
 export class ThreadConversationNotFoundError extends Error {
     constructor() {
-        super('The DM conversaction could not be found');
+        super('Thread conversaction could not be found');
     }
 }
 
 export class ThreadConversationDeletionError extends Error {
     constructor() {
-        super('The DM conversaction could not be deleted');
+        super('Thread conversaction could not be deleted');
     }
 }
 
 export class ThreadConversationUpdatableMessageNotFoundError extends Error {
     constructor() {
-        super('The updatable message could not be found');
+        super('Updatable message could not be found');
+    }
+}
+
+export class ThreadConversationClosedAlreadyError extends Error {
+    constructor() {
+        super('Thread conversaction is already closed');
+    }
+}
+export class ThreadConversationCancelledAlreadyError extends Error {
+    constructor() {
+        super('Thread conversaction is already cancelled');
+    }
+}
+
+export class ThreadConversationAlreadyWaitingApprovalError extends Error {
+    constructor() {
+        super('Thread conversaction is already waiting for approval');
     }
 }
