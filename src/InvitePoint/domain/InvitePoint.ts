d5df08b32acc16e37f28e2a87407facd945aec65
VJA <victorj.alonzo@gmail.com>
2024-10-03 23:14:14 -0400

    Added a interface for the constructor object param.

--- src/InvitePoint/domain/InvitePoint.ts
+++ src/InvitePoint/domain/InvitePoint.ts
@@ -2,7 +2,17 @@ import { IGuild } from "../../Guild/domain/IGuild.js";
 import { createRandomId } from "../../shared/utils/IDGenerator.js";
 import { IInvitePoint } from "./IInvitePoint.js";
 
-export class InvitePoint {
+interface IProps {
+    title?: string
+    description?: string
+    media?: Buffer
+    mediaCodec?: string
+    messageId?: string | null
+    channelId?: string | null
+    guild: IGuild
+}
+
+export class InvitePoint implements IInvitePoint {
     id: string = createRandomId()
     title?: string
     description?: string
@@ -14,14 +24,14 @@ export class InvitePoint {
     guild: IGuild
     createdAt: Date = new Date()
 
-    constructor (props: Omit<IInvitePoint, "id" | "createdAt">) {
+    constructor (props:IProps) {
         this.title = props.title
         this.description = props.description
         this.media = props.media
         this.mediaCodec = props.mediaCodec
         this.messageId = props.messageId
         this.channelId = props.channelId
-        this.guildId = props.guildId
         this.guild = props.guild
+        this.guildId = props.guild.id
     }
 }
