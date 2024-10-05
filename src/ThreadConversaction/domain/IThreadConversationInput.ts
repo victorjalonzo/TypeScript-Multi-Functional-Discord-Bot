de976623ce3b44959b67ff5dce6ec983c9d0efd5
VJA <victorj.alonzo@gmail.com>
2024-10-04 21:05:16 -0400

    Added guildId as a second parameter for some methods.

--- src/ThreadConversaction/domain/IThreadConversationInput.ts
+++ src/ThreadConversaction/domain/IThreadConversationInput.ts
@@ -4,9 +4,10 @@ import { IThreadConversation } from "./IThreadConversation.js";
 export interface IThreadConversationInput {
     create (DMConversaction: IThreadConversation): Promise<Result<IThreadConversation>>
     get(id: string): Promise<Result<IThreadConversation>>
-    getByMember(memberId: string): Promise<Result<IThreadConversation>>
+    getByMember(memberId: string, guildId: string): Promise<Result<IThreadConversation>>
     getActiveOneByThreadChannel(threadChannelId: string, guildId: string): Promise<Result<IThreadConversation>>
-    getActiveOneByMember(memberId: string): Promise<Result<IThreadConversation>>
+    getActiveOneByMember(memberId: string, guildId: string): Promise<Result<IThreadConversation>>
+    getActiveOneByThreadChannel(threadChannelId: string, guildId: string): Promise<Result<IThreadConversation>>
     update(DMConversaction: IThreadConversation): Promise<Result<IThreadConversation>>
     cancel(id: string): Promise<Result<IThreadConversation>>
     close(id: string): Promise<Result<IThreadConversation>>
