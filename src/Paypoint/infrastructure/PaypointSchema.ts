e6f06394527bcfc0212982f317ebaca80e481a0b
VJA <victorj.alonzo@gmail.com>
2024-10-04 21:22:56 -0400

    Added methods to the schema to determine what kind of product the paypoint is based on.

--- src/Paypoint/infrastructure/PaypointSchema.ts
+++ src/Paypoint/infrastructure/PaypointSchema.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema, Document } from 'mongoose';
 import { IPaypoint } from '../domain/IPaypoint.js';
+import { ProductType } from '../../shared/domain/ProductTypeEnums.js';
 
 const PaypointSchema = new Schema<IPaypoint & Document>({
     id: { type: String, required: true },
@@ -13,11 +14,15 @@ const PaypointSchema = new Schema<IPaypoint & Document>({
     channelId: { type: String, default: null },
     createdAt: { type: Date, required: true },
     guildId: { type: String, required: true },
-    guild: { 
-        type: Schema.Types.ObjectId, 
-        ref: "Guilds", 
-        required: true 
-    }
+    guild: { type: Schema.Types.ObjectId, ref: "Guilds", required: true },
 });
+
+PaypointSchema.methods.isBasedOnCreditProduct = function (): boolean {
+    return this.productType == ProductType.CREDIT ? true : false
+}
+
+PaypointSchema.methods.isBasedOnRoleProduct = function (): boolean {
+    return this.productType == ProductType.ROLE ? true : false
+}
 
 export const PaypointModel = mongoose.model<IPaypoint & Document>('Paypoints', PaypointSchema);
