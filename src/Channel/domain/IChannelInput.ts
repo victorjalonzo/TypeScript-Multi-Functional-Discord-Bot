f2c1498f105a11e2b158c74b69fc2e37e3caff32
Victor Alonzo <victorj.alonzo@gmail.com>
2024-10-08 21:37:21 -0400

    Removed the module Channel

--- src/Channel/domain/IChannelInput.ts
+++
@@ -1,8 +0,0 @@
-import {IChannel} from "./IChannel.js";
-import { Result } from "../../shared/domain/Result.js";
-
-export interface IChannelInputPort {
-    create(channel: IChannel): Promise<Result<IChannel>>;
-    update(filter: Record <string, any>, channel: IChannel): Promise<Result<IChannel>>
-    delete(filter: Record <string, any>, channel: IChannel): Promise<Result<Record<string, any>>>;
-}
